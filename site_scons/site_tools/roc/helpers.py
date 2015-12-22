8f13ad7a3de45e953adf969b931dd48fc8022c47
Victor Gaydov <victor@enise.org>
2015-12-23 02:19:50 +0300

    scons: Don't rebuild gengetopt output twice

--- site_scons/site_tools/roc/helpers.py
+++ site_scons/site_tools/roc/helpers.py
@@ -132,15 +132,32 @@ def GenGetOpt(env, source, ver):
     ]
 
     env.Command(target, source, SCons.Action.CommandAction(
-        '%s -F %s --output-dir=%s --set-version=%s < %s' % (
+        '%s -i %s -F %s --output-dir=%s --set-version=%s' % (
             gengetopt,
+            source.srcnode().path,
             source_name,
             os.path.dirname(source.path),
-            ver,
-            source.srcnode().path),
+            ver),
         cmdstr = env.Pretty('GGO', '$SOURCE', 'purple')))
 
-    return [env.Object(target[0])]
+    ret = env.Object(target[0])
+
+    # Workaround to avoid rebuilding returned object file twice.
+    #
+    # What happens here:
+    #  1. When invoked first time, SCons generates .c from .ggo. When
+    #     SCons scans .c files for #includes, .c doesn't exist yet,
+    #     thus it's not scanned.
+    #  2. When invoked second time, SCons scans genarated .c file and
+    #     notes that is depends on generated .h file. Since it's new
+    #     dependency for this file, SCons rebuilds it.
+    #
+    # We force implicit dependency .c -> .h, so that it's present even
+    # if SCons didn't scan .c file yet, and no new dependencies will be
+    # introduced on next invocation.
+    ret[0].add_to_implicit([env.File(target[1])])
+
+    return ret
 
 def ThridParty(env, toolchain, name, includes=[]):
     if not os.path.exists(os.path.join('3rdparty', name, 'commit')):
@@ -151,7 +168,8 @@ def ThridParty(env, toolchain, name, includes=[]):
                     toolchain,
                     name),
                 cmdstr = env.Pretty('GET', name, 'yellow'))):
-            env.Die("can't make `%s', see `3rdparty/%s/build.log' for details" % (name, name))
+            env.Die("can't make `%s', see `3rdparty/%s/build.log' for details" % (
+                name, name))
 
     if not includes:
         includes = ['']
