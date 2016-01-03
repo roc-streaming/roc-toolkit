e5750676e0a8235474b767d3a34de46698beaae6
Victor Gaydov <victor@enise.org>
2016-01-03 21:38:21 +0300

    Travis: print build status

--- scripts/travis/docker.sh
+++ scripts/travis/docker.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
-set -xe
-docker run --rm -ti -u "${UID}" -v "${TRAVIS_BUILD_DIR}":/tmp/roc -w /tmp/roc "$@"
+( set -xe;
+  docker run --rm -ti -u "${UID}" -v "${TRAVIS_BUILD_DIR}":/tmp/roc -w /tmp/roc "$@" )
+error=$?
+if [ $error = 0 ]
+then
+  printf '%-32s \e[1;32m%s\e[m\n' "$1" "OK" >> build.status
+else
+  printf '%-32s \e[1;31m%s\e[m\n' "$1" "FAILED" >> build.status
+fi
+exit $error
