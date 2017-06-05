itsN1X/roc

Refactor packet interfaces (#57)
- extract fields from IPacket, IAudioPacket, and IFECPacket into
  IHeaderOrdering, IHeaderRTP, IHeaderFECFrame, and IPayloadAudio

- remove IAudioPacket and IFECPacket; use aggregated header
  and payload objects instead of inheritance

- replace packet type (which is single value) with packet options
  bitmask

These changes allow a packet to have multiple headers and payloads.
We'll use it to implement various combinations of RTP and FECFRAME
encapsulation.

src/modules/roc_fec/decoder.h
@@ -22,7 +22,6 @@
 #include "roc_packet/ipacket_reader.h"
 #include "roc_packet/ipacket_parser.h"
 #include "roc_packet/ipacket.h"
-#include "roc_packet/ifec_packet.h"
 #include "roc_packet/packet_queue.h"
 
 #include "roc_fec/iblock_decoder.h"
@@ -91,7 +90,7 @@ private:
     packet::PacketQueue fec_queue_;
 
     core::Array<packet::IPacketConstPtr, N_DATA_PACKETS> data_block_;
-    core::Array<packet::IFECPacketConstPtr, N_FEC_PACKETS> fec_block_;
+    core::Array<packet::IPacketConstPtr, N_FEC_PACKETS> fec_block_;
 
     bool is_alive_;
     bool is_started_;

roc-streaming/roc-toolkit

src/modules/roc_fec/decoder.h
@@ -22,7 +22,6 @@
 #include "roc_packet/ipacket_reader.h"
 #include "roc_packet/ipacket_parser.h"
 #include "roc_packet/ipacket.h"
-#include "roc_packet/ifec_packet.h"
 #include "roc_packet/packet_queue.h"
 
 #include "roc_fec/iblock_decoder.h"
@@ -88,7 +87,7 @@ private:
     packet::PacketQueue fec_queue_;
 
     core::Array<packet::IPacketConstPtr, ROC_CONFIG_MAX_FEC_BLOCK_DATA_PACKETS> data_block_;
-    core::Array<packet::IFECPacketConstPtr, ROC_CONFIG_MAX_FEC_BLOCK_REDUNDANT_PACKETS> fec_block_;
+    core::Array<packet::IPacketConstPtr, ROC_CONFIG_MAX_FEC_BLOCK_REDUNDANT_PACKETS> fec_block_;
 
     bool is_alive_;
     bool is_started_;