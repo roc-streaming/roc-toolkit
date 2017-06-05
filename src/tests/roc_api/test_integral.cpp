3741d9bbb9737a72798a4126fdd5733e5dab5415
Mikhail Baranov <baranov.mv@gmail.com>
2017-06-05 11:52:45 +0300

    API integral tests

---
+++ src/tests/roc_api/test_integral.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Mikhail Baranov
+ * Copyright (c) 2015 Victor Gaydov
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <CppUTest/TestHarness.h>
+
+namespace roc {
+namespace test {
+
+TEST_GROUP(api) {
+}
+
+TEST(api, open) {
+    
+}
+
+}
+}
