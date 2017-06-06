418b247161aeaaf4ec788d6cd9147ffc2f2e2041
Mikhail Baranov <baranov.mv@gmail.com>
2017-06-06 22:40:08 +0300

    Comments and small refactoring  C API config

--- src/lib/roc/config.h
+++ src/lib/roc/config.h
@@ -16,23 +16,44 @@
 extern "C" {
 #endif
 
+//! Receiver and sender options.
 enum RocConfigOpt_t {
-    ROC_API_CONF_DISABLE_FEC = 1,
-    ROC_API_CONF_LDPC_CODE = 2,
-    ROC_API_CONF_RESAMPLER_OFF = 4,
-    ROC_API_CONF_INTERLEAVER_OFF = 8,
-    ROC_API_CONF_DISABLE_TIMING = 16
+    //! Turn off resampler in receiver. Might be usefull for 
+    //! exact samples transmition.
+    ROC_API_CONF_RESAMPLER_OFF = (1 << 0),
+
+    //! Turn off interleaver in sender so as to preserve packets order.
+    ROC_API_CONF_INTERLEAVER_OFF = (1 << 1),
+
+    //! Disables timing in receiver or sender to let user use in synchronous environment.
+    //! See further explanaion and usage examples of both cases in documentation.
+    ROC_API_CONF_DISABLE_TIMING = (1 << 2)
 };
 
+//! Configuration of sender or receiver.
 typedef struct roc_config {
+    //! Number of samples of both channels in every packet.
     unsigned int samples_per_packet;
+
+    //! Number of data packets per block.
     unsigned int n_source_packets;
+
+    //! Number of repaier packets (FEC_scheme mustn't be NO_FEC) per block.
     unsigned int n_repair_packets;
+
     //! Session latency as number of samples.
     unsigned int latency;
     //! Timeout after which session is terminated as number of samples.
     unsigned int timeout;
 
+    //! FEC scheme type.
+    enum {
+        ReedSolomon2m = 0,
+        LDPC,
+        NO_FEC
+    } FEC_scheme;
+
+    //! Containment for RocConfigOpt_t flags.
     int options;
 } roc_config;
 
