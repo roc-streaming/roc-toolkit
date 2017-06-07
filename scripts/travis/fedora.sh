17a7a4e707f739ce80131819332dcb66af77f900
Victor Gaydov <victor@enise.org>
2017-06-07 20:24:50 +0300

    Travis fixes

--- scripts/travis/fedora.sh
+++ scripts/travis/fedora.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 set -xe
 scons -Q clean
-scons -Q --enable-werror --with-3rdparty=openfec,cpputest variant=debug test
-scons -Q --enable-werror --with-3rdparty=openfec,cpputest variant=release test
+scons -Q --enable-werror --with-3rdparty=openfec,cpputest test
