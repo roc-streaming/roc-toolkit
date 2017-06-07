0e61078522cdee5dc015c0fd0056d3710eb5bf68
Victor Gaydov <victor@enise.org>
2017-06-07 20:24:50 +0300

    Travis fixes

--- scripts/travis/ubuntu-16.04.sh
+++ scripts/travis/ubuntu-16.04.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 set -xe
+
+scons -Q clean
+for c in gcc-5 clang-3.7
+do
+ scons -Q --enable-werror --enable-sanitizers \
+    --with-3rdparty=openfec,cpputest compiler=$c variant=debug test
+done
+
 scons -Q clean
 for c in gcc-4.8 gcc-4.9 gcc-5 clang-3.6 clang-3.7
 do
-  for v in debug release
+ for v in debug release
   do
-    scons -Q --enable-werror --with-3rdparty=openfec compiler=$c variant=$v test
+    scons -Q --enable-werror --with-3rdparty=openfec \
+        compiler=$c variant=$v test
   done
 done
