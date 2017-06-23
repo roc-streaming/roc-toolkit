770fbd60e06f7442300a8da91de241ff0ed2e1e9
Victor Gaydov <victor@enise.org>
2017-06-23 12:27:38 +0300

    Format code

--- src/modules/roc_audio/timed_writer.cpp
+++ src/modules/roc_audio/timed_writer.cpp
@@ -7,9 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "roc_core/time.h"
 #include "roc_core/log.h"
 #include "roc_core/panic.h"
+#include "roc_core/time.h"
 
 #include "roc_audio/timed_writer.h"
 
