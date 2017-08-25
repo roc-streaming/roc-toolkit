8c861d4735f1507a151d1593c695dc670b1b2b8e
Mikhail Baranov <baranov.mv@gmail.com>
2017-08-25 19:52:30 +0300

    multichannel_resampler: finished first multichannel test

--- src/tests/roc_audio/test_multichannel_resampler.cpp
+++ src/tests/roc_audio/test_multichannel_resampler.cpp
@@ -46,71 +46,47 @@ TEST_GROUP(multichannel_resampler) {
     // Reads signal from the resampler and puts its spectrum into @p spectrum.
     //
     // Spectrum must have twice bigger space than the length of the input signal.
-    // @p nchannel is the channel offset.
-    void get_sample_spectrum(double *spectrum, const size_t sig_len, size_t nchannel = 0) {
+    void get_sample_spectrum(double *spectrum1, double *spectrum2, const size_t sig_len) {
         audio::ISampleBufferPtr buf = new_buffer<InSamples>(sig_len);
         resampler->read(*buf);
         LONGS_EQUAL(sig_len, buf->size());
-        for (size_t i = 0; i < sig_len; ++i) {
-            spectrum[i*2] = (double)buf->data()[i*nChannels + nchannel];
-            spectrum[i*2 + 1] = 0; // imagenary part.
+        size_t i = 0;
+        for (; i < sig_len/nChannels; ++i) {
+            spectrum1[i*2] = (double)buf->data()[i*nChannels];
+            spectrum1[i*2 + 1] = 0; // imagenary part.
+            spectrum2[i*2] = (double)buf->data()[i*nChannels + 1];
+            spectrum2[i*2 + 1] = 0; // imagenary part.
         }
-        FreqSpectrum(spectrum, sig_len);
-    }
-
-    // Generates additive white Gaussian Noise samples with zero mean and a standard deviation of 1.
-    // https://www.embeddedrelated.com/showcode/311.php
-    double AWGN_generator() {
-        const double epsilon = 1.0 / (double)RAND_MAX;
-        double temp1;
-        double temp2 = epsilon;
-        double result;
-        int p;
-
-        p = 1;
-
-        while (p > 0) {
-            temp2 = ( rand() / ( (double)RAND_MAX ) );
-
-            if ( temp2 <= epsilon ) {
-                // temp2 is >= (RAND_MAX / 2)
-                p = 1;
-            } else {
-               p = -1;
-            }
-        }
-
-        temp1 = cos( ( 2.0 * M_PI ) * rand() / ( (double)RAND_MAX ) );
-        result = sqrt( -2.0 * log( temp2 ) ) * temp1;
-
-        return result;    // return the generated random sample to the caller
+        memset(&spectrum1[i*2], 0, (sig_len*2 - i*2)*sizeof(double));
+        memset(&spectrum2[i*2], 0, (sig_len*2 - i*2)*sizeof(double));
+        FreqSpectrum(spectrum1, sig_len/nChannels);
+        FreqSpectrum(spectrum2, sig_len/nChannels);
     }
 };
 
-TEST(multichannel_resampler, mute_second_channel) {
+TEST(multichannel_resampler, two_tones_sep_channels) {
     CHECK(resampler->set_scaling(0.5f));
     const size_t sig_len = 2048;
-    double buff[sig_len*2];
+    double buff1[sig_len*2];
+    double buff2[sig_len*2];
     size_t i;
 
     for (size_t n = 0; n < InSamples/nChannels; n++) {
-        const packet::sample_t s = (packet::sample_t)sin(M_PI/4 * double(n));
-        reader.add(1, s);
-        for (size_t k = 0; k < nChannels - 1; k++) {
-            reader.add(1, 0);
-        }
+        const packet::sample_t s1 = (packet::sample_t)sin(M_PI/4 * double(n));
+        const packet::sample_t s2 = (packet::sample_t)sin(M_PI/8 * double(n));
+        reader.add(1, s1);
+        reader.add(1, s2);
     }
 
     // Put the spectrum of the resampled signal into buff.
     // Odd elements are magnitudes in dB, even elements are phases in radians.
-    get_sample_spectrum(buff, sig_len, 0);
+    get_sample_spectrum(buff1, buff2, sig_len);
 
-    for (i = 0; i < sig_len-1; i += 2) {
-        if (i <= sig_len * 0.90 / 2){
-            CHECK(buff[i] >= -60);
-        } else if (i >= sig_len * 1.00 / 2){
-            CHECK(buff[i] <= -60);
-        }
+    const size_t main_freq_index1 = sig_len / 8 / nChannels;
+    const size_t main_freq_index2 = sig_len / 16 / nChannels;
+    for (i = 0; i < sig_len/2; i += 2) {
+        CHECK((buff1[i] - buff1[main_freq_index1]) <= -75 || i == main_freq_index1);
+        CHECK((buff2[i] - buff2[main_freq_index2]) <= -75 || i == main_freq_index2);
     }
 }
 
