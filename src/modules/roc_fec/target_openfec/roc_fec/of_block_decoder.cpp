mutiadavid/roc

FEC codec type is a parameter of constructor in block codec

src/modules/roc_fec/target_openfec/roc_fec/of_block_decoder.cpp
@@ -21,8 +21,10 @@ const size_t SYMB_SZ = ROC_CONFIG_DEFAULT_PACKET_SIZE;
 
 } // namespace
 
-OF_BlockDecoder::OF_BlockDecoder(core::IByteBufferComposer& composer)
-    : of_inst_(NULL)
+OF_BlockDecoder::OF_BlockDecoder(   core::IByteBufferComposer& composer,
+                                    fec_codec_type_t fec_type)
+    : fec_type_(fec_type)
+    , of_inst_(NULL)
     , of_inst_inited_(false)
     , composer_(composer)
     , buffers_(N_DATA_PACKETS + N_FEC_PACKETS)
@@ -30,7 +32,8 @@ OF_BlockDecoder::OF_BlockDecoder(core::IByteBufferComposer& composer)
     , received_(N_DATA_PACKETS + N_FEC_PACKETS) {
 
     // Use Reed-Solomon Codec.
-    if (codec_id_ == OF_CODEC_REED_SOLOMON_GF_2_M_STABLE){
+    if (fec_type_ == OF_REED_SOLOMON_2_M) {
+        codec_id_ = OF_CODEC_REED_SOLOMON_GF_2_M_STABLE;
         roc_log(LOG_TRACE, "initializing Reed-Solomon decoder");
 
         fec_codec_params_.rs_params_.m = 8;
@@ -38,13 +41,16 @@ OF_BlockDecoder::OF_BlockDecoder(core::IByteBufferComposer& composer)
         of_inst_params_ = (of_parameters_t*)&fec_codec_params_.rs_params_;
 
     // Use LDPC-Staircase.
-    } else {
+    } else if (fec_type_ == OF_LDPC_STAIRCASE) {
+        codec_id_ = OF_CODEC_LDPC_STAIRCASE_STABLE;
         roc_log(LOG_TRACE, "initializing LDPC decoder");
 
         fec_codec_params_.ldpc_params_.prng_seed = 1297501556;
         fec_codec_params_.ldpc_params_.N1 = 7;
 
         of_inst_params_ = (of_parameters_t*)&fec_codec_params_.ldpc_params_; 
+    } else {
+        roc_panic("OF_BlockDecoder: wrong FEC type is chosen.");
     }
 
     of_inst_params_->nb_source_symbols = N_DATA_PACKETS;
@@ -60,21 +66,21 @@ OF_BlockDecoder::~OF_BlockDecoder() {
 
 void OF_BlockDecoder::write(size_t index, const core::IByteBufferConstSlice& buffer) {
     if (index >= N_DATA_PACKETS + N_FEC_PACKETS) {
-        roc_panic("ldpc decoder: index out of bounds: index=%lu, size=%lu",
+        roc_panic("OF_BlockDecoder: index out of bounds: index=%lu, size=%lu",
                   (unsigned long)index, (unsigned long)(N_DATA_PACKETS + N_FEC_PACKETS));
     }
 
     if (!buffer) {
-        roc_panic("ldpc decoder: NULL buffer");
+        roc_panic("OF_BlockDecoder: NULL buffer");
     }
 
     if (buffer.size() != SYMB_SZ) {
-        roc_panic("ldpc decoder: invalid payload size: size=%lu, expected=%lu",
+        roc_panic("OF_BlockDecoder: invalid payload size: size=%lu, expected=%lu",
                   (unsigned long)buffer.size(), (unsigned long)SYMB_SZ);
     }
 
     if (buffers_[index]) {
-        roc_panic("ldpc decoder: can't overwrite buffer: index=%lu",
+        roc_panic("OF_BlockDecoder: can't overwrite buffer: index=%lu",
                   (unsigned long)index);
     }
 
@@ -117,23 +123,24 @@ void OF_BlockDecoder::reset() {
 
     if (OF_STATUS_OK != of_create_codec_instance(
                             &of_inst_, codec_id_, OF_DECODER, 0)) {
-        roc_panic("ldpc decoder: of_create_codec_instance() failed");
+        roc_panic("OF_BlockDecoder: of_create_codec_instance() failed");
     }
 
     roc_panic_if(of_inst_ == NULL);
 
     if (OF_STATUS_OK
         != of_set_fec_parameters(of_inst_, of_inst_params_)) {
-        roc_panic("ldpc decoder: of_set_fec_parameters() failed");
+        roc_panic("OF_BlockDecoder: of_set_fec_parameters() failed");
     }
 
     of_inst_inited_ = true;
 
-    if (OF_STATUS_OK
-        != of_set_callback_functions(of_inst_, source_cb_,
+    if (OF_STATUS_OK != of_set_callback_functions(of_inst_, source_cb_,
+            // OpenFEC doesn't repair fec-packets in case of Reed-Solomon FEC
+            // and prints curses to the console if we give him the callback for that.
             codec_id_ == OF_CODEC_REED_SOLOMON_GF_2_M_STABLE ? NULL : repair_cb_,
             (void*)this)) {
-        roc_panic("ldpc decoder: of_set_callback_functions() failed");
+        roc_panic("OF_BlockDecoder: of_set_callback_functions() failed");
     }
 
     for (size_t i = 0; i < buffers_.size(); ++i) {
@@ -174,7 +181,7 @@ void OF_BlockDecoder::report_() {
         return;
     }
 
-    roc_log(LOG_TRACE, "ldpc decoder: repaired %u/%u/%u %s %s",
+    roc_log(LOG_TRACE, "OF_BlockDecoder: repaired %u/%u/%u %s %s",
             (unsigned)n_repaired,      //
             (unsigned)n_lost,          //
             (unsigned)buffers_.size(), //
@@ -190,7 +197,7 @@ void* OF_BlockDecoder::make_buffer_(const size_t index) {
         buffers_[index] = *buffer;
         return buffer->data();
     } else {
-        roc_log(LOG_TRACE, "ldpc decoder: can't allocate buffer");
+        roc_log(LOG_TRACE, "OF_BlockDecoder: can't allocate buffer");
         return NULL;
     }
 }

roc-streaming/roc-toolkit

src/modules/roc_fec/target_openfec/roc_fec/of_block_decoder.cpp
@@ -21,8 +21,10 @@ const size_t SYMB_SZ = ROC_CONFIG_DEFAULT_PACKET_SIZE;
 
 } // namespace
 
-OF_BlockDecoder::OF_BlockDecoder(core::IByteBufferComposer& composer)
-    : of_inst_(NULL)
+OF_BlockDecoder::OF_BlockDecoder(   core::IByteBufferComposer& composer,
+                                    fec_codec_type_t fec_type)
+    : fec_type_(fec_type)
+    , of_inst_(NULL)
     , of_inst_inited_(false)
     , composer_(composer)
     , buffers_(N_DATA_PACKETS + N_FEC_PACKETS)
@@ -31,7 +33,8 @@ OF_BlockDecoder::OF_BlockDecoder(core::IByteBufferComposer& composer)
     roc_log(LogDebug, "initializing ldpc decoder");
 
     // Use Reed-Solomon Codec.
-    if (codec_id_ == OF_CODEC_REED_SOLOMON_GF_2_M_STABLE){
+    if (fec_type_ == OF_REED_SOLOMON_2_M) {
+        codec_id_ = OF_CODEC_REED_SOLOMON_GF_2_M_STABLE;
         roc_log(LOG_TRACE, "initializing Reed-Solomon decoder");
 
         fec_codec_params_.rs_params_.m = 8;
@@ -39,13 +42,16 @@ OF_BlockDecoder::OF_BlockDecoder(core::IByteBufferComposer& composer)
         of_inst_params_ = (of_parameters_t*)&fec_codec_params_.rs_params_;
 
     // Use LDPC-Staircase.
-    } else {
+    } else if (fec_type_ == OF_LDPC_STAIRCASE) {
+        codec_id_ = OF_CODEC_LDPC_STAIRCASE_STABLE;
         roc_log(LOG_TRACE, "initializing LDPC decoder");
 
         fec_codec_params_.ldpc_params_.prng_seed = 1297501556;
         fec_codec_params_.ldpc_params_.N1 = 7;
 
         of_inst_params_ = (of_parameters_t*)&fec_codec_params_.ldpc_params_; 
+    } else {
+        roc_panic("OF_BlockDecoder: wrong FEC type is chosen.");
     }
 
     of_inst_params_->nb_source_symbols = N_DATA_PACKETS;
@@ -61,21 +67,21 @@ OF_BlockDecoder::~OF_BlockDecoder() {
 
 void OF_BlockDecoder::write(size_t index, const core::IByteBufferConstSlice& buffer) {
     if (index >= N_DATA_PACKETS + N_FEC_PACKETS) {
-        roc_panic("ldpc decoder: index out of bounds: index=%lu, size=%lu",
+        roc_panic("OF_BlockDecoder: index out of bounds: index=%lu, size=%lu",
                   (unsigned long)index, (unsigned long)(N_DATA_PACKETS + N_FEC_PACKETS));
     }
 
     if (!buffer) {
-        roc_panic("ldpc decoder: NULL buffer");
+        roc_panic("OF_BlockDecoder: NULL buffer");
     }
 
     if (buffer.size() != SYMB_SZ) {
-        roc_panic("ldpc decoder: invalid payload size: size=%lu, expected=%lu",
+        roc_panic("OF_BlockDecoder: invalid payload size: size=%lu, expected=%lu",
                   (unsigned long)buffer.size(), (unsigned long)SYMB_SZ);
     }
 
     if (buffers_[index]) {
-        roc_panic("ldpc decoder: can't overwrite buffer: index=%lu",
+        roc_panic("OF_BlockDecoder: can't overwrite buffer: index=%lu",
                   (unsigned long)index);
     }
 
@@ -118,23 +124,24 @@ void OF_BlockDecoder::reset() {
 
     if (OF_STATUS_OK != of_create_codec_instance(
                             &of_inst_, codec_id_, OF_DECODER, 0)) {
-        roc_panic("ldpc decoder: of_create_codec_instance() failed");
+        roc_panic("OF_BlockDecoder: of_create_codec_instance() failed");
     }
 
     roc_panic_if(of_inst_ == NULL);
 
     if (OF_STATUS_OK
         != of_set_fec_parameters(of_inst_, of_inst_params_)) {
-        roc_panic("ldpc decoder: of_set_fec_parameters() failed");
+        roc_panic("OF_BlockDecoder: of_set_fec_parameters() failed");
     }
 
     of_inst_inited_ = true;
 
-    if (OF_STATUS_OK
-        != of_set_callback_functions(of_inst_, source_cb_,
+    if (OF_STATUS_OK != of_set_callback_functions(of_inst_, source_cb_,
+            // OpenFEC doesn't repair fec-packets in case of Reed-Solomon FEC
+            // and prints curses to the console if we give him the callback for that.
             codec_id_ == OF_CODEC_REED_SOLOMON_GF_2_M_STABLE ? NULL : repair_cb_,
             (void*)this)) {
-        roc_panic("ldpc decoder: of_set_callback_functions() failed");
+        roc_panic("OF_BlockDecoder: of_set_callback_functions() failed");
     }
 
     for (size_t i = 0; i < buffers_.size(); ++i) {
@@ -175,7 +182,7 @@ void OF_BlockDecoder::report_() {
         return;
     }
 
-    roc_log(LogDebug, "ldpc decoder: repaired %u/%u/%u %s %s",
+    roc_log(LogDebug, "lOF_BlockDecoder repaired %u/%u/%u %s %s",
             (unsigned)n_repaired,      //
             (unsigned)n_lost,          //
             (unsigned)buffers_.size(), //
@@ -191,7 +198,7 @@ void* OF_BlockDecoder::make_buffer_(const size_t index) {
         buffers_[index] = *buffer;
         return buffer->data();
     } else {
-        roc_log(LogDebug, "ldpc decoder: can't allocate buffer");
+        roc_log(LogDebugE, "OF_BlockDecoder: can't allocate buffer");
         return NULL;
     }
 }