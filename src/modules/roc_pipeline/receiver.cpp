itsN1X/roc

Rename server -> receiver, client -> sender

src/modules/roc_pipeline/receiver.cpp
@@ -11,14 +11,14 @@
 #include "roc_core/log.h"
 #include "roc_datagram/address_to_str.h"
 
-#include "roc_pipeline/server.h"
+#include "roc_pipeline/receiver.h"
 
 namespace roc {
 namespace pipeline {
 
-Server::Server(datagram::IDatagramReader& datagram_reader,
-               audio::ISampleBufferWriter& audio_writer,
-               const ServerConfig& config)
+Receiver::Receiver(datagram::IDatagramReader& datagram_reader,
+                   audio::ISampleBufferWriter& audio_writer,
+                   const ReceiverConfig& config)
     : config_(config)
     , n_channels_(packet::num_channels(config_.channels))
     , channel_muxer_(config_.channels, *config_.sample_buffer_composer)
@@ -28,23 +28,23 @@ Server::Server(datagram::IDatagramReader& datagram_reader,
     , session_manager_(config_, channel_muxer_) {
     //
     if (n_channels_ == 0) {
-        roc_panic("server: channel mask is zero");
+        roc_panic("receiver: channel mask is zero");
     }
 
     if (config_.samples_per_tick == 0) {
-        roc_panic("server: # of samples per tick is zero");
+        roc_panic("receiver: # of samples per tick is zero");
     }
 
     if (!config_.byte_buffer_composer) {
-        roc_panic("server: byte buffer composer is null");
+        roc_panic("receiver: byte buffer composer is null");
     }
 
     if (!config_.sample_buffer_composer) {
-        roc_panic("server: sample buffer composer is null");
+        roc_panic("receiver: sample buffer composer is null");
     }
 
     if (!config_.session_pool) {
-        roc_panic("server: session pool is null");
+        roc_panic("receiver: session pool is null");
     }
 
     if (config_.options & EnableTiming) {
@@ -53,16 +53,16 @@ Server::Server(datagram::IDatagramReader& datagram_reader,
     }
 }
 
-size_t Server::num_sessions() const {
+size_t Receiver::num_sessions() const {
     return session_manager_.num_sessions();
 }
 
-void Server::add_port(const datagram::Address& address, packet::IPacketParser& parser) {
+void Receiver::add_port(const datagram::Address& address, packet::IPacketParser& parser) {
     session_manager_.add_port(address, parser);
 }
 
-void Server::run() {
-    roc_log(LOG_DEBUG, "server: starting thread: output_latency=%u session_latency=%u",
+void Receiver::run() {
+    roc_log(LOG_DEBUG, "receiver: starting thread: output_latency=%u session_latency=%u",
             (unsigned)config_.output_latency, (unsigned)config_.session_latency);
 
     while (!stop_) {
@@ -71,17 +71,17 @@ void Server::run() {
         }
     }
 
-    roc_log(LOG_DEBUG, "server: finishing thread");
+    roc_log(LOG_DEBUG, "receiver: finishing thread");
 
     // Write EOF.
     audio_writer_->write(audio::ISampleBufferConstSlice());
 }
 
-void Server::stop() {
+void Receiver::stop() {
     stop_ = true;
 }
 
-bool Server::tick() {
+bool Receiver::tick() {
     for (size_t n = 0; n < config_.max_sessions * config_.max_session_packets; n++) {
         if (datagram::IDatagramConstPtr dgm = datagram_reader_.read()) {
             session_manager_.route(*dgm);
@@ -96,7 +96,7 @@ bool Server::tick() {
 
     audio::ISampleBufferPtr buffer = config_.sample_buffer_composer->compose();
     if (!buffer) {
-        roc_log(LOG_ERROR, "server: can't compose sample buffer");
+        roc_log(LOG_ERROR, "receiver: can't compose sample buffer");
         return false;
     }
 

src/modules/roc_pipeline/sender.cpp
@@ -10,16 +10,16 @@
 #include "roc_core/panic.h"
 #include "roc_core/log.h"
 
-#include "roc_pipeline/client.h"
+#include "roc_pipeline/sender.h"
 
 namespace roc {
 namespace pipeline {
 
-Client::Client(audio::ISampleBufferReader& audio_reader,
+Sender::Sender(audio::ISampleBufferReader& audio_reader,
                datagram::IDatagramWriter& datagram_writer,
                datagram::IDatagramComposer& datagram_composer,
                packet::IPacketComposer& packet_composer,
-               const ClientConfig& config)
+               const SenderConfig& config)
     : config_(config)
     , packet_sender_(datagram_writer, datagram_composer)
     , packet_composer_(packet_composer)
@@ -28,16 +28,16 @@ Client::Client(audio::ISampleBufferReader& audio_reader,
     , datagram_writer_(datagram_writer) {
 }
 
-void Client::set_sender(const datagram::Address& address) {
+void Sender::set_sender(const datagram::Address& address) {
     packet_sender_.set_sender(address);
 }
 
-void Client::set_receiver(const datagram::Address& address) {
+void Sender::set_receiver(const datagram::Address& address) {
     packet_sender_.set_receiver(address);
 }
 
-void Client::run() {
-    roc_log(LOG_DEBUG, "client: starting thread");
+void Sender::run() {
+    roc_log(LOG_DEBUG, "sender: starting thread");
 
     for (;;) {
         if (!tick()) {
@@ -45,26 +45,26 @@ void Client::run() {
         }
     }
 
-    roc_log(LOG_DEBUG, "client: finishing thread");
+    roc_log(LOG_DEBUG, "sender: finishing thread");
 
     flush();
 
     datagram_writer_.write(NULL);
 }
 
-bool Client::tick() {
+bool Sender::tick() {
     audio::ISampleBufferConstSlice buffer = audio_reader_.read();
 
     if (buffer) {
         audio_writer_.write(buffer);
     } else {
-        roc_log(LOG_DEBUG, "client: audio reader returned null");
+        roc_log(LOG_DEBUG, "sender: audio reader returned null");
     }
 
     return (bool)buffer;
 }
 
-void Client::flush() {
+void Sender::flush() {
     if (splitter_) {
         splitter_->flush();
     }
@@ -74,7 +74,7 @@ void Client::flush() {
     }
 }
 
-audio::ISampleBufferWriter* Client::make_audio_writer_() {
+audio::ISampleBufferWriter* Sender::make_audio_writer_() {
     packet::IPacketWriter* packet_writer = make_packet_writer_();
     roc_panic_if(!packet_writer);
 
@@ -90,7 +90,7 @@ audio::ISampleBufferWriter* Client::make_audio_writer_() {
     return audio_writer;
 }
 
-packet::IPacketWriter* Client::make_packet_writer_() {
+packet::IPacketWriter* Sender::make_packet_writer_() {
     packet::IPacketWriter* packet_writer = &packet_sender_;
 
     if (config_.random_loss_rate || config_.random_delay_rate) {
@@ -112,15 +112,15 @@ packet::IPacketWriter* Client::make_packet_writer_() {
 }
 
 #ifdef ROC_TARGET_OPENFEC
-packet::IPacketWriter* Client::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
+packet::IPacketWriter* Sender::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
     new (fec_ldpc_encoder_) fec::LDPC_BlockEncoder(*config_.byte_buffer_composer);
 
     return new (fec_encoder_)
         fec::Encoder(*fec_ldpc_encoder_, *packet_writer, packet_composer_);
 }
 #else
-packet::IPacketWriter* Client::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
-    roc_log(LOG_ERROR, "client: OpenFEC support not enabled, disabling fec encoder");
+packet::IPacketWriter* Sender::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
+    roc_log(LOG_ERROR, "sender: OpenFEC support not enabled, disabling fec encoder");
     return packet_writer;
 }
 #endif

src/tests/roc_pipeline/test_sender_receiver.cpp
@@ -13,8 +13,8 @@
 #include "roc_rtp/composer.h"
 #include "roc_rtp/parser.h"
 #include "roc_datagram/datagram_queue.h"
-#include "roc_pipeline/client.h"
-#include "roc_pipeline/server.h"
+#include "roc_pipeline/sender.h"
+#include "roc_pipeline/receiver.h"
 
 #include "test_sample_stream.h"
 #include "test_sample_queue.h"
@@ -25,13 +25,13 @@ namespace test {
 
 using namespace pipeline;
 
-TEST_GROUP(client_server) {
+TEST_GROUP(sender_receiver) {
     enum {
         // Sending port.
-        ClientPort = 501,
+        SenderPort = 501,
 
         // Receiving port.
-        ServerPort = 502,
+        ReceiverPort = 502,
 
         // Number of samples in every channel per packet.
         PktSamples = ROC_CONFIG_DEFAULT_PACKET_SAMPLES,
@@ -58,8 +58,8 @@ TEST_GROUP(client_server) {
     rtp::Composer packet_composer;
     rtp::Parser packet_parser;
 
-    core::ScopedPtr<Client> client;
-    core::ScopedPtr<Server> server;
+    core::ScopedPtr<Sender> sender;
+    core::ScopedPtr<Receiver> receiver;
 
     void setup() {
     }
@@ -70,23 +70,23 @@ TEST_GROUP(client_server) {
         LONGS_EQUAL(0, network.size());
     }
 
-    void init_client(int options, size_t random_loss = 0) {
-        ClientConfig config;
+    void init_sender(int options, size_t random_loss = 0) {
+        SenderConfig config;
 
         config.options = options;
         config.channels = ChannelMask;
         config.samples_per_packet = PktSamples;
         config.random_loss_rate = random_loss;
 
-        client.reset(
-            new Client(input, network, datagram_composer, packet_composer, config));
+        sender.reset(
+            new Sender(input, network, datagram_composer, packet_composer, config));
 
-        client->set_sender(new_address(ClientPort));
-        client->set_receiver(new_address(ServerPort));
+        sender->set_sender(new_address(SenderPort));
+        sender->set_receiver(new_address(ReceiverPort));
     }
 
-    void init_server(int options) {
-        ServerConfig config;
+    void init_receiver(int options) {
+        ReceiverConfig config;
 
         config.options = options;
         config.channels = ChannelMask;
@@ -95,12 +95,12 @@ TEST_GROUP(client_server) {
         config.output_latency = 0;
         config.samples_per_tick = BufSamples;
 
-        server.reset(new Server(network, output, config));
+        receiver.reset(new Receiver(network, output, config));
 
-        server->add_port(new_address(ServerPort), packet_parser);
+        receiver->add_port(new_address(ReceiverPort), packet_parser);
     }
 
-    void flow_client_server() {
+    void flow_sender_receiver() {
         SampleStream si;
 
         for (size_t n = 0; n < MaxBuffers; n++) {
@@ -110,17 +110,17 @@ TEST_GROUP(client_server) {
         LONGS_EQUAL(MaxBuffers, input.size());
 
         while (input.size() != 0) {
-            CHECK(client->tick());
+            CHECK(sender->tick());
         }
 
-        client->flush();
+        sender->flush();
 
         CHECK(network.size() >= MaxBuffers * BufSamples / PktSamples);
 
         SampleStream so;
 
         for (size_t n = 0; n < MaxBuffers; n++) {
-            CHECK(server->tick());
+            CHECK(receiver->tick());
 
             LONGS_EQUAL(1, output.size());
 
@@ -133,53 +133,53 @@ TEST_GROUP(client_server) {
     }
 };
 
-TEST(client_server, bare) {
-    init_client(0);
-    init_server(0);
-    flow_client_server();
+TEST(sender_receiver, bare) {
+    init_sender(0);
+    init_receiver(0);
+    flow_sender_receiver();
 }
 
-TEST(client_server, interleaving) {
-    init_client(EnableInterleaving);
-    init_server(0);
-    flow_client_server();
+TEST(sender_receiver, interleaving) {
+    init_sender(EnableInterleaving);
+    init_receiver(0);
+    flow_sender_receiver();
 }
 
 #ifdef ROC_TARGET_OPENFEC
-TEST(client_server, ldpc_only_client) {
-    init_client(EnableFEC);
-    init_server(0);
-    flow_client_server();
+TEST(sender_receiver, ldpc_only_sender) {
+    init_sender(EnableFEC);
+    init_receiver(0);
+    flow_sender_receiver();
 }
 
-TEST(client_server, ldpc_only_server) {
-    init_client(0);
-    init_server(EnableFEC);
-    flow_client_server();
+TEST(sender_receiver, ldpc_only_receiver) {
+    init_sender(0);
+    init_receiver(EnableFEC);
+    flow_sender_receiver();
 }
 
-TEST(client_server, ldpc) {
-    init_client(EnableFEC);
-    init_server(EnableFEC);
-    flow_client_server();
+TEST(sender_receiver, ldpc) {
+    init_sender(EnableFEC);
+    init_receiver(EnableFEC);
+    flow_sender_receiver();
 }
 
-TEST(client_server, ldpc_interleaving) {
-    init_client(EnableFEC | EnableInterleaving);
-    init_server(EnableFEC);
-    flow_client_server();
+TEST(sender_receiver, ldpc_interleaving) {
+    init_sender(EnableFEC | EnableInterleaving);
+    init_receiver(EnableFEC);
+    flow_sender_receiver();
 }
 
-IGNORE_TEST(client_server, ldpc_random_loss) {
-    init_client(EnableFEC, RandomLoss);
-    init_server(EnableFEC);
-    flow_client_server();
+IGNORE_TEST(sender_receiver, ldpc_random_loss) {
+    init_sender(EnableFEC, RandomLoss);
+    init_receiver(EnableFEC);
+    flow_sender_receiver();
 }
 
-IGNORE_TEST(client_server, ldpc_interleaving_random_loss) {
-    init_client(EnableFEC | EnableInterleaving, RandomLoss);
-    init_server(EnableFEC);
-    flow_client_server();
+IGNORE_TEST(sender_receiver, ldpc_interleaving_random_loss) {
+    init_sender(EnableFEC | EnableInterleaving, RandomLoss);
+    init_receiver(EnableFEC);
+    flow_sender_receiver();
 }
 #endif // ROC_TARGET_OPENFEC
 

roc-streaming/roc-toolkit

src/modules/roc_pipeline/receiver.cpp
@@ -11,14 +11,14 @@
 #include "roc_core/log.h"
 #include "roc_datagram/address_to_str.h"
 
-#include "roc_pipeline/server.h"
+#include "roc_pipeline/receiver.h"
 
 namespace roc {
 namespace pipeline {
 
-Server::Server(datagram::IDatagramReader& datagram_reader,
-               audio::ISampleBufferWriter& audio_writer,
-               const ServerConfig& config)
+Receiver::Receiver(datagram::IDatagramReader& datagram_reader,
+                   audio::ISampleBufferWriter& audio_writer,
+                   const ReceiverConfig& config)
     : config_(config)
     , n_channels_(packet::num_channels(config_.channels))
     , channel_muxer_(config_.channels, *config_.sample_buffer_composer)
@@ -28,23 +28,23 @@ Server::Server(datagram::IDatagramReader& datagram_reader,
     , session_manager_(config_, channel_muxer_) {
     //
     if (n_channels_ == 0) {
-        roc_panic("server: channel mask is zero");
+        roc_panic("receiver: channel mask is zero");
     }
 
     if (config_.samples_per_tick == 0) {
-        roc_panic("server: # of samples per tick is zero");
+        roc_panic("receiver: # of samples per tick is zero");
     }
 
     if (!config_.byte_buffer_composer) {
-        roc_panic("server: byte buffer composer is null");
+        roc_panic("receiver: byte buffer composer is null");
     }
 
     if (!config_.sample_buffer_composer) {
-        roc_panic("server: sample buffer composer is null");
+        roc_panic("receiver: sample buffer composer is null");
     }
 
     if (!config_.session_pool) {
-        roc_panic("server: session pool is null");
+        roc_panic("receiver: session pool is null");
     }
 
     if (config_.options & EnableTiming) {
@@ -53,16 +53,16 @@ Server::Server(datagram::IDatagramReader& datagram_reader,
     }
 }
 
-size_t Server::num_sessions() const {
+size_t Receiver::num_sessions() const {
     return session_manager_.num_sessions();
 }
 
-void Server::add_port(const datagram::Address& address, packet::IPacketParser& parser) {
+void Receiver::add_port(const datagram::Address& address, packet::IPacketParser& parser) {
     session_manager_.add_port(address, parser);
 }
 
-void Server::run() {
-    roc_log(LogInfo, "server: starting thread: output_latency=%u session_latency=%u",
+void Receiver::run() {
+    roc_log(LogInfo, "receiver: starting thread: output_latency=%u session_latency=%u",
             (unsigned)config_.output_latency, (unsigned)config_.session_latency);
 
     while (!stop_) {
@@ -71,17 +71,17 @@ void Server::run() {
         }
     }
 
-    roc_log(LogInfo, "server: finishing thread");
+    roc_log(LogInfo, "receiver: finishing thread");
 
     // Write EOF.
     audio_writer_->write(audio::ISampleBufferConstSlice());
 }
 
-void Server::stop() {
+void Receiver::stop() {
     stop_ = true;
 }
 
-bool Server::tick() {
+bool Receiver::tick() {
     for (size_t n = 0; n < config_.max_sessions * config_.max_session_packets; n++) {
         if (datagram::IDatagramConstPtr dgm = datagram_reader_.read()) {
             session_manager_.route(*dgm);
@@ -96,7 +96,7 @@ bool Server::tick() {
 
     audio::ISampleBufferPtr buffer = config_.sample_buffer_composer->compose();
     if (!buffer) {
-        roc_log(LogError, "server: can't compose sample buffer");
+        roc_log(LogError, "receiver: can't compose sample buffer");
         return false;
     }
 

src/modules/roc_pipeline/sender.cpp
@@ -10,16 +10,16 @@
 #include "roc_core/panic.h"
 #include "roc_core/log.h"
 
-#include "roc_pipeline/client.h"
+#include "roc_pipeline/sender.h"
 
 namespace roc {
 namespace pipeline {
 
-Client::Client(audio::ISampleBufferReader& audio_reader,
+Sender::Sender(audio::ISampleBufferReader& audio_reader,
                datagram::IDatagramWriter& datagram_writer,
                datagram::IDatagramComposer& datagram_composer,
                packet::IPacketComposer& packet_composer,
-               const ClientConfig& config)
+               const SenderConfig& config)
     : config_(config)
     , packet_sender_(datagram_writer, datagram_composer)
     , packet_composer_(packet_composer)
@@ -28,16 +28,16 @@ Client::Client(audio::ISampleBufferReader& audio_reader,
     , datagram_writer_(datagram_writer) {
 }
 
-void Client::set_sender(const datagram::Address& address) {
+void Sender::set_sender(const datagram::Address& address) {
     packet_sender_.set_sender(address);
 }
 
-void Client::set_receiver(const datagram::Address& address) {
+void Sender::set_receiver(const datagram::Address& address) {
     packet_sender_.set_receiver(address);
 }
 
-void Client::run() {
-    roc_log(LogInfo, "client: starting thread");
+void Sender::run() {
+    roc_log(LogInfo, "sender: starting thread");
 
     for (;;) {
         if (!tick()) {
@@ -45,26 +45,26 @@ void Client::run() {
         }
     }
 
-    roc_log(LogInfo, "client: finishing thread");
+    roc_log(LogInfo, "sender: finishing thread");
 
     flush();
 
     datagram_writer_.write(NULL);
 }
 
-bool Client::tick() {
+bool Sender::tick() {
     audio::ISampleBufferConstSlice buffer = audio_reader_.read();
 
     if (buffer) {
         audio_writer_.write(buffer);
     } else {
-        roc_log(LogInfo, "client: audio reader returned null");
+        roc_log(LogInfo, "sender: audio reader returned null");
     }
 
     return (bool)buffer;
 }
 
-void Client::flush() {
+void Sender::flush() {
     if (splitter_) {
         splitter_->flush();
     }
@@ -74,7 +74,7 @@ void Client::flush() {
     }
 }
 
-audio::ISampleBufferWriter* Client::make_audio_writer_() {
+audio::ISampleBufferWriter* Sender::make_audio_writer_() {
     packet::IPacketWriter* packet_writer = make_packet_writer_();
     roc_panic_if(!packet_writer);
 
@@ -90,7 +90,7 @@ audio::ISampleBufferWriter* Client::make_audio_writer_() {
     return audio_writer;
 }
 
-packet::IPacketWriter* Client::make_packet_writer_() {
+packet::IPacketWriter* Sender::make_packet_writer_() {
     packet::IPacketWriter* packet_writer = &packet_sender_;
 
     if (config_.random_loss_rate || config_.random_delay_rate) {
@@ -113,7 +113,7 @@ packet::IPacketWriter* Client::make_packet_writer_() {
 }
 
 #ifdef ROC_TARGET_OPENFEC
-packet::IPacketWriter* Client::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
+packet::IPacketWriter* Sender::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
     new (fec_ldpc_encoder_) fec::OFBlockEncoder(config_.fec,
                                                     *config_.byte_buffer_composer);
 
@@ -121,8 +121,8 @@ packet::IPacketWriter* Client::make_fec_encoder_(packet::IPacketWriter* packet_w
         fec::Encoder(*fec_ldpc_encoder_, *packet_writer, packet_composer_);
 }
 #else
-packet::IPacketWriter* Client::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
-    roc_log(LogError, "client: OpenFEC support not enabled, disabling fec encoder");
+packet::IPacketWriter* Sender::make_fec_encoder_(packet::IPacketWriter* packet_writer) {
+    roc_log(LogError, "sender: OpenFEC support not enabled, disabling fec encoder");
     return packet_writer;
 }
 #endif

src/tests/roc_pipeline/test_sender_receiver.cpp
@@ -13,8 +13,8 @@
 #include "roc_rtp/composer.h"
 #include "roc_rtp/parser.h"
 #include "roc_datagram/datagram_queue.h"
-#include "roc_pipeline/client.h"
-#include "roc_pipeline/server.h"
+#include "roc_pipeline/sender.h"
+#include "roc_pipeline/receiver.h"
 
 #include "test_sample_stream.h"
 #include "test_sample_queue.h"
@@ -31,13 +31,13 @@ const fec::CodecType Fec = fec::ReedSolomon2m;
 
 using namespace pipeline;
 
-TEST_GROUP(client_server) {
+TEST_GROUP(sender_receiver) {
     enum {
         // Sending port.
-        ClientPort = 501,
+        SenderPort = 501,
 
         // Receiving port.
-        ServerPort = 502,
+        ReceiverPort = 502,
 
         // Number of samples in every channel per packet.
         PktSamples = ROC_CONFIG_DEFAULT_PACKET_SAMPLES,
@@ -64,8 +64,8 @@ TEST_GROUP(client_server) {
     rtp::Composer packet_composer;
     rtp::Parser packet_parser;
 
-    core::ScopedPtr<Client> client;
-    core::ScopedPtr<Server> server;
+    core::ScopedPtr<Sender> sender;
+    core::ScopedPtr<Receiver> receiver;
 
     void setup() {
     }
@@ -76,9 +76,9 @@ TEST_GROUP(client_server) {
         LONGS_EQUAL(0, network.size());
     }
 
-    void init_client(int options, fec::CodecType codec = fec::NoCodec,
+    void init_sender(int options, fec::CodecType codec = fec::NoCodec,
                                 size_t random_loss = 0) {
-        ClientConfig config;
+        SenderConfig config;
 
         config.options = options;
         config.channels = ChannelMask;
@@ -88,15 +88,15 @@ TEST_GROUP(client_server) {
         config.fec.n_source_packets = 20;
         config.fec.n_repair_packets = 10;
 
-        client.reset(
-            new Client(input, network, datagram_composer, packet_composer, config));
+        sender.reset(
+            new Sender(input, network, datagram_composer, packet_composer, config));
 
-        client->set_sender(new_address(ClientPort));
-        client->set_receiver(new_address(ServerPort));
+        sender->set_sender(new_address(SenderPort));
+        sender->set_receiver(new_address(ReceiverPort));
     }
 
-    void init_server(int options, fec::CodecType codec = fec::NoCodec) {
-        ServerConfig config;
+    void init_receiver(int options, fec::CodecType codec = fec::NoCodec) {
+        ReceiverConfig config;
 
         config.options = options;
         config.channels = ChannelMask;
@@ -108,12 +108,12 @@ TEST_GROUP(client_server) {
         config.fec.n_source_packets = 20;
         config.fec.n_repair_packets = 10;
 
-        server.reset(new Server(network, output, config));
+        receiver.reset(new Receiver(network, output, config));
 
-        server->add_port(new_address(ServerPort), packet_parser);
+        receiver->add_port(new_address(ReceiverPort), packet_parser);
     }
 
-    void flow_client_server() {
+    void flow_sender_receiver() {
         SampleStream si;
 
         for (size_t n = 0; n < MaxBuffers; n++) {
@@ -123,17 +123,17 @@ TEST_GROUP(client_server) {
         LONGS_EQUAL(MaxBuffers, input.size());
 
         while (input.size() != 0) {
-            CHECK(client->tick());
+            CHECK(sender->tick());
         }
 
-        client->flush();
+        sender->flush();
 
         CHECK(network.size() >= MaxBuffers * BufSamples / PktSamples);
 
         SampleStream so;
 
         for (size_t n = 0; n < MaxBuffers; n++) {
-            CHECK(server->tick());
+            CHECK(receiver->tick());
 
             LONGS_EQUAL(1, output.size());
 
@@ -146,53 +146,53 @@ TEST_GROUP(client_server) {
     }
 };
 
-TEST(client_server, bare) {
-    init_client(0);
-    init_server(0);
-    flow_client_server();
+TEST(sender_receiver, bare) {
+    init_sender(0);
+    init_receiver(0);
+    flow_sender_receiver();
 }
 
-TEST(client_server, interleaving) {
-    init_client(EnableInterleaving);
-    init_server(0);
-    flow_client_server();
+TEST(sender_receiver, interleaving) {
+    init_sender(EnableInterleaving);
+    init_receiver(0);
+    flow_sender_receiver();
 }
 
 #ifdef ROC_TARGET_OPENFEC
-TEST(client_server, fec_only_client) {
-    init_client(0, Fec);
-    init_server(0);
-    flow_client_server();
+TEST(sender_receiver, fec_only_sender) {
+    init_sender(0, Fec);
+    init_receiver(0);
+    flow_sender_receiver();
 }
 
-TEST(client_server, fec_only_server) {
-    init_client(0);
-    init_server(0, Fec);
-    flow_client_server();
+TEST(sender_receiver, fec_only_receiver) {
+    init_sender(0);
+    init_receiver(0, Fec);
+    flow_sender_receiver();
 }
 
-TEST(client_server, fec_both) {
-    init_client(0, Fec);
-    init_server(0, Fec);
-    flow_client_server();
+TEST(sender_receiver, fec_both) {
+    init_sender(0, Fec);
+    init_receiver(0, Fec);
+    flow_sender_receiver();
 }
 
-TEST(client_server, fec_interleaving) {
-    init_client(EnableInterleaving, Fec);
-    init_server(0, Fec);
-    flow_client_server();
+TEST(sender_receiver, fec_interleaving) {
+    init_sender(EnableInterleaving, Fec);
+    init_receiver(0, Fec);
+    flow_sender_receiver();
 }
 
-TEST(client_server, fec_random_loss) {
-    init_client(0, Fec, RandomLoss);
-    init_server(0, Fec);
-    flow_client_server();
+TEST(sender_receiver, fec_random_loss) {
+    init_sender(0, Fec, RandomLoss);
+    init_receiver(0, Fec);
+    flow_sender_receiver();
 }
 
-TEST(client_server, fec_interleaving_random_loss) {
-    init_client(EnableInterleaving, Fec, RandomLoss);
-    init_server(0, Fec);
-    flow_client_server();
+TEST(sender_receiver, fec_interleaving_random_loss) {
+    init_sender(EnableInterleaving, Fec, RandomLoss);
+    init_receiver(0, Fec);
+    flow_sender_receiver();
 }
 #endif // ROC_TARGET_OPENFEC
 