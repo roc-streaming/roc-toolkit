93be921176cc45b387cac2075f716aa99f8b2215
Victor Gaydov <victor@enise.org>
2019-01-09 12:18:58 +0300

    Replace roc-resample with roc-conv

--- src/tools/roc_conv/main.cpp
+++ src/tools/roc_conv/main.cpp
@@ -18,7 +18,7 @@
 #include "roc_sndio/sox_reader.h"
 #include "roc_sndio/sox_writer.h"
 
-#include "roc_resample/cmdline.h"
+#include "roc_conv/cmdline.h"
 
 using namespace roc;
 
@@ -52,14 +52,12 @@ int main(int argc, char** argv) {
     }
 
     sndio::SoxReader reader(pool, Channels, chunk_size, 0);
-
     if (!reader.open(args.input_arg, NULL)) {
         roc_log(LogError, "can't open input file: %s", args.input_arg);
         return 1;
     }
-
     if (!reader.is_file()) {
-        roc_log(LogError, "not a file file: %s", args.input_arg);
+        roc_log(LogError, "not a file: %s", args.input_arg);
         return 1;
     }
 
@@ -82,47 +80,46 @@ int main(int argc, char** argv) {
     }
 
     audio::ResamplerConfig resampler_config;
-
-    if (args.interp_given) {
-        resampler_config.window_interp = (size_t)args.interp_arg;
+    if (args.resampler_interp_given) {
+        resampler_config.window_interp = (size_t)args.resampler_interp_arg;
     }
-
-    if (args.window_given) {
-        resampler_config.window_size = (size_t)args.window_arg;
+    if (args.resampler_window_given) {
+        resampler_config.window_size = (size_t)args.resampler_window_arg;
     }
-
-    if (args.frame_given) {
-        resampler_config.frame_size = (size_t)args.frame_arg;
+    if (args.resampler_frame_given) {
+        resampler_config.frame_size = (size_t)args.resampler_frame_arg;
     }
 
     audio::ResamplerWriter resampler(*writer, pool, allocator, resampler_config, Channels);
-    if (!resampler.valid()) {
-        roc_log(LogError, "can't create resampler");
-        return 1;
+    if (!args.no_resampling_flag) {
+        if (!resampler.valid()) {
+            roc_log(LogError, "can't create resampler");
+            return 1;
+        }
+        if (!resampler.set_scaling((float)reader.sample_rate() / writer_sample_rate)) {
+            roc_log(LogError, "can't set resampler scaling");
+            return 1;
+        }
+        writer = &resampler;
     }
 
-    if (!resampler.set_scaling((float)reader.sample_rate() / writer_sample_rate)) {
-        roc_log(LogError, "can't set resampler scaling");
-        return 1;
-    }
+    audio::ProfilingWriter profiler(*writer, Channels, reader.sample_rate());
+    writer = &profiler;
 
     if (args.output_given) {
         if (!output.open(args.output_arg, NULL)) {
             roc_log(LogError, "can't open output file: %s", args.output_arg);
             return 1;
         }
-
         if (!output.is_file()) {
-            roc_log(LogError, "not a file file: %s", args.output_arg);
+            roc_log(LogError, "not a file: %s", args.output_arg);
             return 1;
         }
     }
 
-    audio::ProfilingWriter profiler(resampler, Channels, reader.sample_rate());
-
     int status = 1;
 
-    if (reader.start(profiler)) {
+    if (reader.start(*writer)) {
         reader.join();
         status = 0;
         roc_log(LogInfo, "done");
