68f20cd892e7bad60720184223b1f1df5fb84dae
Victor Gaydov <victor@enise.org>
2019-06-17 18:58:42 +0300

    travis: Add cross-aarch64-linux-android:api28

---
+++ scripts/travis/android/aarch64-linux-android.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -euxo pipefail
+
+TOOLCHAIN="aarch64-linux-android"
+
+scons -Q clean
+
+scons -Q \
+    --enable-werror \
+    --disable-tools \
+    --disable-examples \
+    --build-3rdparty=uv,openfec,cpputest \
+    --compiler=clang \
+    --host=${TOOLCHAIN}
