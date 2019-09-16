e64aa8d202fd0d88dec2b5b1a7c751bc589594e1
Hrishikesh Suresh <hrishikesh123s@gmail.com>
2019-09-16 18:42:50 +0530

    adding a no-op backtrace implementation

---
+++ src/modules/roc_core/target_nobacktrace/roc_core/backtrace.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <stdio.h>
+
+
+#include "roc_core/backtrace.h"
+
+namespace roc {
+namespace core {
+
+namespace {
+
+enum { MaxDepth = 128, MaxLen = 128 };
+
+}
+
+void print_backtrace() 
+{
+}
+
+void print_backtrace_emergency() 
+{
+}
+
+} // namespace core
+} // namespace roc
