edada08aaf32c54b25f5ab92ce2ecdddb688ead9
Victor Gaydov <victor@enise.org>
2019-10-21 12:28:22 +0300

    Cosmetic renames in packet::Address

--- src/modules/roc_pipeline/target_stdio/roc_pipeline/parse_port.cpp
+++ src/modules/roc_pipeline/target_stdio/roc_pipeline/parse_port.cpp
@@ -129,12 +129,12 @@ bool parse_port(PortType type, const char* input, PortConfig& result) {
 
         memcpy(addr6, addr + 1, addrlen - 2);
 
-        if (!result.address.set_host_ipv6(addr6, (int)port_num)) {
+        if (!result.address.set_host_port_ipv6(addr6, (int)port_num)) {
             roc_log(LogError, "parse port: bad IPv6 address: %s", addr6);
             return false;
         }
     } else {
-        if (!result.address.set_host_ipv4(addr, (int)port_num)) {
+        if (!result.address.set_host_port_ipv4(addr, (int)port_num)) {
             roc_log(LogError, "parse port: bad IPv4 address: %s", addr);
             return false;
         }
