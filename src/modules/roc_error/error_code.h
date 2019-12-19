2e51f70f384383dca3531e059d4f7a822e19829c
Victor Gaydov <victor@enise.org>
2019-12-19 13:15:23 +0300

    Add roc_error module

---
+++ src/modules/roc_error/error_code.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+//! @file roc_error/error_code.h
+//! @brief Error codes.
+
+#ifndef ROC_ERROR_ERROR_CODE_H_
+#define ROC_ERROR_ERROR_CODE_H_
+
+namespace roc {
+namespace error {
+
+//! Error code.
+enum ErrorCode {
+    ErrUnknown = -1 //!< Unknown error.
+};
+
+} // namespace error
+} // namespace roc
+
+#endif // ROC_ERROR_ERROR_CODE_H_
