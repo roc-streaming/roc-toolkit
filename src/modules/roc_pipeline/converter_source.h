33d7161c73aec60fad689a0d5633820a547760cb
Victor Gaydov <victor@enise.org>
2020-01-20 12:41:40 +0300

    Add pipeline::ConverterSource
    Issue: #313

---
+++ src/modules/roc_pipeline/converter_source.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+//! @file roc_pipeline/converter_source.h
+//! @brief Converter source pipeline.
+
+#ifndef ROC_PIPELINE_CONVERTER_SOURCE_H_
+#define ROC_PIPELINE_CONVERTER_SOURCE_H_
+
+#include "roc_audio/iresampler.h"
+#include "roc_audio/poison_reader.h"
+#include "roc_audio/resampler_map.h"
+#include "roc_audio/resampler_profile.h"
+#include "roc_audio/resampler_reader.h"
+#include "roc_core/buffer_pool.h"
+#include "roc_core/scoped_ptr.h"
+#include "roc_pipeline/config.h"
+#include "roc_sndio/isource.h"
+
+namespace roc {
+namespace pipeline {
+
+//! Converter source pipeline.
+//! @remarks
+//   - input: frames
+//!  - output: frames
+class ConverterSource : public sndio::ISource, public core::NonCopyable<> {
+public:
+    //! Initialize.
+    ConverterSource(const ConverterConfig& config,
+                    sndio::ISource& input_source,
+                    core::BufferPool<audio::sample_t>& pool,
+                    core::IAllocator& allocator);
+
+    //! Check if the pipeline was successfully constructed.
+    bool valid();
+
+    //! Get sink sample rate.
+    virtual size_t sample_rate() const;
+
+    //! Check if the sink has own clock.
+    virtual bool has_clock() const;
+
+    //! Get current source state.
+    virtual State state() const;
+
+    //! Pause reading.
+    virtual void pause();
+
+    //! Resume paused reading.
+    virtual bool resume();
+
+    //! Restart reading from the beginning.
+    virtual bool restart();
+
+    //! Read frame.
+    virtual bool read(audio::Frame&);
+
+private:
+    core::ScopedPtr<audio::PoisonReader> resampler_poisoner_;
+    core::ScopedPtr<audio::ResamplerReader> resampler_reader_;
+    core::ScopedPtr<audio::IResampler> resampler_;
+
+    core::ScopedPtr<audio::PoisonReader> pipeline_poisoner_;
+
+    sndio::ISource& input_source_;
+    audio::IReader* audio_reader_;
+
+    ConverterConfig config_;
+};
+
+} // namespace pipeline
+} // namespace roc
+
+#endif // ROC_PIPELINE_CONVERTER_SOURCE_H_
---
+++ src/tests/roc_pipeline/test_converter_source.cpp
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2019 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <CppUTest/TestHarness.h>
+
+#include "roc_core/buffer_pool.h"
+#include "roc_core/heap_allocator.h"
+#include "roc_pipeline/converter_source.h"
+
+#include "test_frame_reader.h"
+#include "test_mock_source.h"
+
+namespace roc {
+namespace pipeline {
+
+namespace {
+
+enum {
+    MaxBufSize = 1000,
+
+    SampleRate = 44100,
+    ChMask = 0x3,
+    NumCh = 2,
+
+    SamplesPerFrame = 20,
+    ManyFrames = 30
+};
+
+core::HeapAllocator allocator;
+core::BufferPool<audio::sample_t> sample_buffer_pool(allocator, MaxBufSize, true);
+
+} // namespace
+
+TEST_GROUP(converter_source) {
+    ConverterConfig config;
+
+    void setup() {
+        config.input_channels = ChMask;
+        config.output_channels = ChMask;
+
+        config.input_sample_rate = SampleRate;
+        config.output_sample_rate = SampleRate;
+
+        config.internal_frame_size = MaxBufSize;
+
+        config.resampling = false;
+        config.poisoning = true;
+    }
+};
+
+TEST(converter_source, state) {
+    MockSource mock_source;
+
+    ConverterSource converter(config, mock_source, sample_buffer_pool, allocator);
+    CHECK(converter.valid());
+
+    mock_source.set_state(sndio::ISource::Active);
+    CHECK(converter.state() == sndio::ISource::Active);
+
+    mock_source.set_state(sndio::ISource::Inactive);
+    CHECK(converter.state() == sndio::ISource::Inactive);
+}
+
+TEST(converter_source, pause_resume) {
+    MockSource mock_source;
+
+    ConverterSource converter(config, mock_source, sample_buffer_pool, allocator);
+    CHECK(converter.valid());
+
+    converter.pause();
+    CHECK(converter.state() == sndio::ISource::Paused);
+    CHECK(mock_source.state() == sndio::ISource::Paused);
+
+    CHECK(converter.resume());
+    CHECK(converter.state() == sndio::ISource::Active);
+    CHECK(mock_source.state() == sndio::ISource::Active);
+}
+
+TEST(converter_source, pause_restart) {
+    MockSource mock_source;
+
+    ConverterSource converter(config, mock_source, sample_buffer_pool, allocator);
+    CHECK(converter.valid());
+
+    converter.pause();
+    CHECK(converter.state() == sndio::ISource::Paused);
+    CHECK(mock_source.state() == sndio::ISource::Paused);
+
+    CHECK(converter.restart());
+    CHECK(converter.state() == sndio::ISource::Active);
+    CHECK(mock_source.state() == sndio::ISource::Active);
+}
+
+TEST(converter_source, read) {
+    MockSource mock_source;
+    mock_source.add(ManyFrames * SamplesPerFrame * NumCh);
+
+    ConverterSource converter(config, mock_source, sample_buffer_pool, allocator);
+    CHECK(converter.valid());
+
+    FrameReader frame_reader(converter, sample_buffer_pool);
+
+    for (size_t nf = 0; nf < ManyFrames; nf++) {
+        frame_reader.read_samples(SamplesPerFrame * NumCh, 1);
+    }
+
+    UNSIGNED_LONGS_EQUAL(mock_source.num_remaining(), 0);
+}
+
+TEST(converter_source, eof) {
+    MockSource mock_source;
+
+    ConverterSource converter(config, mock_source, sample_buffer_pool, allocator);
+    CHECK(converter.valid());
+
+    core::Slice<audio::sample_t> samples(
+        new (sample_buffer_pool) core::Buffer<audio::sample_t>(sample_buffer_pool));
+    samples.resize(SamplesPerFrame * NumCh);
+
+    audio::Frame frame(samples.data(), samples.size());
+
+    mock_source.add(SamplesPerFrame * NumCh);
+    CHECK(converter.read(frame));
+    CHECK(!converter.read(frame));
+}
+
+TEST(converter_source, frame_size_small) {
+    enum { SamplesPerSmallFrame = SamplesPerFrame / 2 - 3 };
+
+    MockSource mock_source;
+    mock_source.add(ManyFrames * SamplesPerSmallFrame * NumCh);
+
+    ConverterSource converter(config, mock_source, sample_buffer_pool, allocator);
+    CHECK(converter.valid());
+
+    FrameReader frame_reader(converter, sample_buffer_pool);
+
+    for (size_t nf = 0; nf < ManyFrames; nf++) {
+        frame_reader.read_samples(SamplesPerSmallFrame * NumCh, 1);
+    }
+
+    UNSIGNED_LONGS_EQUAL(mock_source.num_remaining(), 0);
+}
+
+TEST(converter_source, frame_size_large) {
+    enum { SamplesPerLargeFrame = SamplesPerFrame * 2 + 3 };
+
+    MockSource mock_source;
+    mock_source.add(ManyFrames * SamplesPerLargeFrame * NumCh);
+
+    ConverterSource converter(config, mock_source, sample_buffer_pool, allocator);
+    CHECK(converter.valid());
+
+    FrameReader frame_reader(converter, sample_buffer_pool);
+
+    for (size_t nf = 0; nf < ManyFrames; nf++) {
+        frame_reader.read_samples(SamplesPerLargeFrame * NumCh, 1);
+    }
+
+    UNSIGNED_LONGS_EQUAL(mock_source.num_remaining(), 0);
+}
+
+} // namespace pipeline
+} // namespace roc
