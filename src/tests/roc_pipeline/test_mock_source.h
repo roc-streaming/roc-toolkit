4e65cc209f405e261e40ecd30cf4875195af5303
Victor Gaydov <victor@enise.org>
2020-01-20 12:41:40 +0300

    Add pipeline::ConverterSource
    Issue: #313

---
+++ src/tests/roc_pipeline/test_mock_source.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef ROC_PIPELINE_TEST_MOCK_SOURCE_H_
+#define ROC_PIPELINE_TEST_MOCK_SOURCE_H_
+
+#include <CppUTest/TestHarness.h>
+
+#include "test_helpers.h"
+
+#include "roc_sndio/isource.h"
+
+namespace roc {
+namespace pipeline {
+
+class MockSource : public sndio::ISource {
+public:
+    MockSource()
+        : state_(Active)
+        , pos_(0)
+        , size_(0) {
+    }
+
+    virtual size_t sample_rate() const {
+        return 0;
+    }
+
+    virtual bool has_clock() const {
+        return false;
+    }
+
+    void set_state(State st) {
+        state_ = st;
+    }
+
+    virtual State state() const {
+        return state_;
+    }
+
+    virtual void pause() {
+        state_ = Paused;
+    }
+
+    virtual bool resume() {
+        state_ = Active;
+        return true;
+    }
+
+    virtual bool restart() {
+        state_ = Active;
+        return true;
+    }
+
+    virtual bool read(audio::Frame& frame) {
+        if (pos_ == size_) {
+            return false;
+        }
+
+        size_t ns = frame.size();
+        if (ns > size_ - pos_) {
+            ns = size_ - pos_;
+        }
+
+        if (ns > 0) {
+            memcpy(frame.data(), samples_ + pos_, ns * sizeof(audio::sample_t));
+            pos_ += ns;
+        }
+
+        if (ns < frame.size()) {
+            memset(frame.data() + ns, 0, (frame.size() - ns) * sizeof(audio::sample_t));
+        }
+
+        return true;
+    }
+
+    void add(size_t sz) {
+        CHECK(size_ + sz <= MaxSz);
+
+        for (size_t n = 0; n < sz; n++) {
+            samples_[size_] = nth_sample((uint8_t)size_);
+            size_++;
+        }
+    }
+
+    size_t num_remaining() const {
+        return size_ - pos_;
+    }
+
+private:
+    enum { MaxSz = 256 * 1024 };
+
+    State state_;
+
+    audio::sample_t samples_[MaxSz];
+
+    size_t pos_;
+    size_t size_;
+};
+
+} // namespace pipeline
+} // namespace roc
+
+#endif // ROC_PIPELINE_TEST_MOCK_SOURCE_H_
