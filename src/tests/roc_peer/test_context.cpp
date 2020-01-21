081f157e6fde1fe04cb88c28cecc3dc602616240
Victor Gaydov <victor@enise.org>
2020-01-21 15:44:18 +0300

    Add roc_peer tests
    Issue: #326

---
+++ src/tests/roc_peer/test_context.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <CppUTest/TestHarness.h>
+
+#include "roc_peer/context.h"
+#include "roc_peer/receiver.h"
+#include "roc_peer/sender.h"
+
+namespace roc {
+namespace peer {
+
+TEST_GROUP(context) {};
+
+TEST(context, reference_counting) {
+    ContextConfig context_config;
+    Context context(context_config);
+
+    CHECK(context.valid());
+    CHECK(!context.is_used());
+
+    {
+        pipeline::SenderConfig sender_config;
+        Sender sender(context, sender_config);
+
+        CHECK(context.is_used());
+    }
+
+    CHECK(!context.is_used());
+
+    {
+        pipeline::ReceiverConfig receiver_config;
+        Receiver receiver(context, receiver_config);
+
+        CHECK(context.is_used());
+    }
+
+    CHECK(!context.is_used());
+}
+
+} // namespace peer
+} // namespace roc
