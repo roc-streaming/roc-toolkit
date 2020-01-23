bdbd4679fd471cf2c2fce0581c19a40c1c02cb4c
Victor Gaydov <victor@enise.org>
2020-01-23 22:12:43 +0300

    Make ResamplerMap singleton

--- src/modules/roc_audio/resampler_map.h
+++ src/modules/roc_audio/resampler_map.h
@@ -7,30 +7,41 @@
  */
 
 //! @file roc_audio/resampler_map.h
-//! @brief Resampler Map.
+//! @brief Resampler map.
 
 #ifndef ROC_AUDIO_RESAMPLER_MAP_H_
 #define ROC_AUDIO_RESAMPLER_MAP_H_
 
-#include "roc_core/stddefs.h"
-
 #include "roc_audio/iresampler.h"
 #include "roc_audio/resampler_config.h"
 #include "roc_core/iallocator.h"
+#include "roc_core/noncopyable.h"
 #include "roc_core/scoped_ptr.h"
+#include "roc_core/singleton.h"
+#include "roc_core/stddefs.h"
 #include "roc_packet/units.h"
 
 namespace roc {
 namespace audio {
 //! Factory class for IResampler objects, according to the ResamplerBackend input
-class ResamplerMap {
+class ResamplerMap : public core::NonCopyable<> {
 public:
+    //! Get instance.
+    static ResamplerMap& instance() {
+        return core::Singleton<ResamplerMap>::instance();
+    }
+
     //! Method to instantiate and return a pointer to a IResampler object
     IResampler* new_resampler(ResamplerBackend resampler_backend,
                               core::IAllocator& allocator,
                               const ResamplerConfig& config,
                               packet::channel_mask_t channels,
                               size_t frame_size);
+
+private:
+    friend class core::Singleton<ResamplerMap>;
+
+    ResamplerMap();
 };
 
 } // namespace audio
