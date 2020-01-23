510df674f990425593dd00dbd82642f9feb7eb2d
Victor Gaydov <victor@enise.org>
2020-01-23 21:16:10 +0300

    Rework sender pipeline ports
    Issue: #330

--- src/modules/roc_pipeline/sender_sink.h
+++ src/modules/roc_pipeline/sender_sink.h
@@ -12,12 +12,11 @@
 #ifndef ROC_PIPELINE_SENDER_SINK_H_
 #define ROC_PIPELINE_SENDER_SINK_H_
 
+#include "roc_audio/fanout.h"
 #include "roc_audio/iframe_encoder.h"
 #include "roc_audio/iresampler.h"
 #include "roc_audio/packetizer.h"
 #include "roc_audio/poison_writer.h"
-#include "roc_audio/resampler_map.h"
-#include "roc_audio/resampler_writer.h"
 #include "roc_core/buffer_pool.h"
 #include "roc_core/iallocator.h"
 #include "roc_core/noncopyable.h"
@@ -30,7 +29,7 @@
 #include "roc_packet/packet_pool.h"
 #include "roc_packet/router.h"
 #include "roc_pipeline/config.h"
-#include "roc_pipeline/sender_port.h"
+#include "roc_pipeline/sender_port_group.h"
 #include "roc_rtp/format_map.h"
 #include "roc_sndio/isink.h"
 
@@ -38,17 +37,14 @@ namespace roc {
 namespace pipeline {
 
 //! Sender sink pipeline.
+//! Thread-safe.
 //! @remarks
-//   - input: frames
+//!  - input: frames
 //!  - output: packets
 class SenderSink : public sndio::ISink, public core::NonCopyable<> {
 public:
     //! Initialize.
     SenderSink(const SenderConfig& config,
-               const PortConfig& source_port,
-               packet::IWriter& source_writer,
-               const PortConfig& repair_port,
-               packet::IWriter& repair_writer,
                const fec::CodecMap& codec_map,
                const rtp::FormatMap& format_map,
                packet::PacketPool& packet_pool,
@@ -57,7 +53,30 @@ public:
                core::IAllocator& allocator);
 
     //! Check if the pipeline was successfully constructed.
-    bool valid();
+    bool valid() const;
+
+    //! Port group identifier.
+    typedef uintptr_t PortGroupID;
+
+    //! Port identifier.
+    typedef uintptr_t PortID;
+
+    //! Add new port group.
+    //! @returns
+    //!  non-zero identifier on success or zero on error.
+    PortGroupID add_port_group();
+
+    //! Add port to group.
+    PortID add_port(PortGroupID port_group,
+                    address::EndpointType port_type,
+                    const pipeline::PortConfig& port_config);
+
+    //! Attach port to packet writer.
+    void set_port_writer(PortID port_id, packet::IWriter& port_writer);
+
+    //! Check if port group is fully configured.
+    //! This is true when all necessary ports are added and attached to writers.
+    bool port_group_configured(PortGroupID port_group) const;
 
     //! Get sink sample rate.
     virtual size_t sample_rate() const;
@@ -72,22 +91,20 @@ public:
     virtual void write(audio::Frame& frame);
 
 private:
-    core::ScopedPtr<SenderPort> source_port_;
-    core::ScopedPtr<SenderPort> repair_port_;
+    const SenderConfig config_;
 
-    core::ScopedPtr<packet::Router> router_;
+    const fec::CodecMap& codec_map_;
+    const rtp::FormatMap& format_map_;
 
-    core::ScopedPtr<packet::Interleaver> interleaver_;
+    packet::PacketPool& packet_pool_;
+    core::BufferPool<uint8_t>& byte_buffer_pool_;
+    core::BufferPool<audio::sample_t>& sample_buffer_pool_;
 
-    core::ScopedPtr<fec::IBlockEncoder> fec_encoder_;
-    core::ScopedPtr<fec::Writer> fec_writer_;
+    core::IAllocator& allocator_;
 
-    core::ScopedPtr<audio::IFrameEncoder> payload_encoder_;
-    core::ScopedPtr<audio::Packetizer> packetizer_;
+    core::List<SenderPortGroup> port_groups_;
 
-    core::ScopedPtr<audio::PoisonWriter> resampler_poisoner_;
-    core::ScopedPtr<audio::ResamplerWriter> resampler_writer_;
-    core::ScopedPtr<audio::IResampler> resampler_;
+    audio::Fanout fanout_;
 
     core::ScopedPtr<audio::PoisonWriter> pipeline_poisoner_;
 
@@ -95,10 +112,10 @@ private:
 
     audio::IWriter* audio_writer_;
 
-    SenderConfig config_;
-
     packet::timestamp_t timestamp_;
     size_t num_channels_;
+
+    core::Mutex mutex_;
 };
 
 } // namespace pipeline
