61b4c04592b9da4f71575deb24f2c9677c06ffb1
Victor Gaydov <victor@enise.org>
2020-01-23 14:43:41 +0300

    Add audio fanout

---
+++ src/tests/roc_audio/test_mock_writer.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef ROC_AUDIO_TEST_MOCK_WRITER_H_
+#define ROC_AUDIO_TEST_MOCK_WRITER_H_
+
+#include <CppUTest/TestHarness.h>
+
+#include "roc_audio/iwriter.h"
+#include "roc_core/stddefs.h"
+
+namespace roc {
+namespace audio {
+
+class MockWriter : public IWriter {
+public:
+    MockWriter()
+        : pos_(0)
+        , size_(0) {
+    }
+
+    virtual void write(Frame& frame) {
+        CHECK(size_ + frame.size() <= MaxSz);
+
+        memcpy(samples_ + size_, frame.data(), frame.size() * sizeof(sample_t));
+        size_ += frame.size();
+    }
+
+    sample_t get() {
+        CHECK(pos_ < size_);
+
+        return samples_[pos_++];
+    }
+
+    size_t num_unread() const {
+        return size_ - pos_;
+    }
+
+private:
+    enum { MaxSz = 64 * 1024 };
+
+    sample_t samples_[MaxSz];
+    size_t pos_;
+    size_t size_;
+};
+
+} // namespace audio
+} // namespace roc
+
+#endif // ROC_AUDIO_TEST_MOCK_WRITER_H_
