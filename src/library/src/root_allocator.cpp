7805e6a63a268b8a7e9eea9386fa3d1442431a66
Victor Gaydov <victor@enise.org>
2020-01-26 20:40:01 +0300

    API: introduce root allocator

---
+++ src/library/src/root_allocator.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "root_allocator.h"
+
+namespace roc {
+namespace api {
+
+core::HeapAllocator root_allocator;
+
+} // namespace api
+} // namespace roc
