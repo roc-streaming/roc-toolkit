8b5e4ad18ad0e65e886bfbc5c87c1798f3d508dd
Victor Gaydov <victor@enise.org>
2020-02-04 14:57:36 +0300

    Use opaque port handle in netio::EventLoop

--- src/tests/roc_netio/test_event_loop.cpp
+++ src/tests/roc_netio/test_event_loop.cpp
@@ -42,6 +42,106 @@ TEST(event_loop, init) {
     CHECK(event_loop.valid());
 }
 
+TEST(event_loop, add) {
+    packet::ConcurrentQueue queue;
+
+    EventLoop event_loop(packet_pool, buffer_pool, allocator);
+    CHECK(event_loop.valid());
+
+    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
+    address::SocketAddr rx_addr = make_address("0.0.0.0", 0);
+
+    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
+
+    EventLoop::PortHandle snd = event_loop.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    EventLoop::PortHandle rcv = event_loop.add_udp_receiver(rx_addr, queue);
+    CHECK(rcv);
+    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
+}
+
+TEST(event_loop, add_remove) {
+    packet::ConcurrentQueue queue;
+
+    EventLoop event_loop(packet_pool, buffer_pool, allocator);
+    CHECK(event_loop.valid());
+
+    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
+    address::SocketAddr rx_addr = make_address("0.0.0.0", 0);
+
+    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
+
+    EventLoop::PortHandle snd = event_loop.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    EventLoop::PortHandle rcv = event_loop.add_udp_receiver(rx_addr, queue);
+    CHECK(rcv);
+    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
+
+    event_loop.remove_port(snd);
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    event_loop.remove_port(rcv);
+    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
+}
+
+TEST(event_loop, add_remove_add) {
+    EventLoop event_loop(packet_pool, buffer_pool, allocator);
+    CHECK(event_loop.valid());
+
+    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
+
+    EventLoop::PortHandle snd = event_loop.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    event_loop.remove_port(snd);
+    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
+
+    snd = event_loop.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+}
+
+TEST(event_loop, add_duplicate) {
+    packet::ConcurrentQueue queue;
+
+    EventLoop event_loop(packet_pool, buffer_pool, allocator);
+    CHECK(event_loop.valid());
+
+    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
+    address::SocketAddr rx_addr = make_address("0.0.0.0", 0);
+
+    EventLoop::PortHandle snd = event_loop.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    CHECK(!event_loop.add_udp_sender(tx_addr, NULL));
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    CHECK(!event_loop.add_udp_receiver(tx_addr, queue));
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    EventLoop::PortHandle rcv = event_loop.add_udp_receiver(rx_addr, queue);
+    CHECK(rcv);
+    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
+
+    CHECK(!event_loop.add_udp_sender(rx_addr, NULL));
+    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
+
+    CHECK(!event_loop.add_udp_receiver(rx_addr, queue));
+    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
+
+    event_loop.remove_port(snd);
+    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
+
+    event_loop.remove_port(rcv);
+    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
+}
+
 TEST(event_loop, bind_any) {
     packet::ConcurrentQueue queue;
 
@@ -52,11 +152,16 @@ TEST(event_loop, bind_any) {
     address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
     address::SocketAddr rx_addr = make_address("0.0.0.0", 0);
 
-    CHECK(event_loop.add_udp_sender(tx_addr));
-    CHECK(event_loop.add_udp_receiver(rx_addr, queue));
+    EventLoop::PortHandle snd = event_loop.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    CHECK(tx_addr.port() != 0);
 
-    event_loop.remove_port(tx_addr);
-    event_loop.remove_port(rx_addr);
+    EventLoop::PortHandle rcv = event_loop.add_udp_receiver(rx_addr, queue);
+    CHECK(rcv);
+    CHECK(rx_addr.port() != 0);
+
+    event_loop.remove_port(snd);
+    event_loop.remove_port(rcv);
 }
 
 TEST(event_loop, bind_lo) {
@@ -69,11 +174,16 @@ TEST(event_loop, bind_lo) {
     address::SocketAddr tx_addr = make_address("127.0.0.1", 0);
     address::SocketAddr rx_addr = make_address("127.0.0.1", 0);
 
-    CHECK(event_loop.add_udp_sender(tx_addr));
-    CHECK(event_loop.add_udp_receiver(rx_addr, queue));
+    EventLoop::PortHandle snd = event_loop.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    CHECK(tx_addr.port() != 0);
 
-    event_loop.remove_port(tx_addr);
-    event_loop.remove_port(rx_addr);
+    EventLoop::PortHandle rcv = event_loop.add_udp_receiver(rx_addr, queue);
+    CHECK(rcv);
+    CHECK(rx_addr.port() != 0);
+
+    event_loop.remove_port(snd);
+    event_loop.remove_port(rcv);
 }
 
 TEST(event_loop, bind_addrinuse) {
@@ -85,100 +195,19 @@ TEST(event_loop, bind_addrinuse) {
     address::SocketAddr tx_addr = make_address("127.0.0.1", 0);
     address::SocketAddr rx_addr = make_address("127.0.0.1", 0);
 
-    CHECK(event_loop1.add_udp_sender(tx_addr));
-    CHECK(event_loop1.add_udp_receiver(rx_addr, queue));
+    EventLoop::PortHandle snd = event_loop1.add_udp_sender(tx_addr, NULL);
+    CHECK(snd);
+    CHECK(tx_addr.port() != 0);
+
+    EventLoop::PortHandle rcv = event_loop1.add_udp_receiver(rx_addr, queue);
+    CHECK(rcv);
+    CHECK(rx_addr.port() != 0);
 
     EventLoop event_loop2(packet_pool, buffer_pool, allocator);
     CHECK(event_loop2.valid());
 
-    CHECK(!event_loop2.add_udp_sender(tx_addr));
+    CHECK(!event_loop2.add_udp_sender(tx_addr, NULL));
     CHECK(!event_loop2.add_udp_receiver(rx_addr, queue));
-}
-
-TEST(event_loop, add) {
-    packet::ConcurrentQueue queue;
-
-    EventLoop event_loop(packet_pool, buffer_pool, allocator);
-
-    CHECK(event_loop.valid());
-
-    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
-    address::SocketAddr rx_addr = make_address("0.0.0.0", 0);
-
-    CHECK(event_loop.add_udp_sender(tx_addr));
-    CHECK(event_loop.add_udp_receiver(rx_addr, queue));
-}
-
-TEST(event_loop, add_remove) {
-    packet::ConcurrentQueue queue;
-
-    EventLoop event_loop(packet_pool, buffer_pool, allocator);
-
-    CHECK(event_loop.valid());
-
-    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
-    address::SocketAddr rx_addr = make_address("0.0.0.0", 0);
-
-    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
-
-    CHECK(event_loop.add_udp_sender(tx_addr));
-    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
-
-    CHECK(event_loop.add_udp_receiver(rx_addr, queue));
-    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
-
-    event_loop.remove_port(tx_addr);
-    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
-
-    event_loop.remove_port(rx_addr);
-    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
-}
-
-TEST(event_loop, add_remove_add) {
-    EventLoop event_loop(packet_pool, buffer_pool, allocator);
-
-    CHECK(event_loop.valid());
-
-    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
-
-    CHECK(event_loop.add_udp_sender(tx_addr));
-    event_loop.remove_port(tx_addr);
-    CHECK(event_loop.add_udp_sender(tx_addr));
-}
-
-TEST(event_loop, add_duplicate) {
-    packet::ConcurrentQueue queue;
-
-    EventLoop event_loop(packet_pool, buffer_pool, allocator);
-
-    CHECK(event_loop.valid());
-
-    address::SocketAddr tx_addr = make_address("0.0.0.0", 0);
-    address::SocketAddr rx_addr = make_address("0.0.0.0", 0);
-
-    CHECK(event_loop.add_udp_sender(tx_addr));
-    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
-
-    CHECK(!event_loop.add_udp_sender(tx_addr));
-    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
-
-    CHECK(!event_loop.add_udp_receiver(tx_addr, queue));
-    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
-
-    CHECK(event_loop.add_udp_receiver(rx_addr, queue));
-    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
-
-    CHECK(!event_loop.add_udp_sender(rx_addr));
-    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
-
-    CHECK(!event_loop.add_udp_receiver(rx_addr, queue));
-    UNSIGNED_LONGS_EQUAL(2, event_loop.num_ports());
-
-    event_loop.remove_port(tx_addr);
-    UNSIGNED_LONGS_EQUAL(1, event_loop.num_ports());
-
-    event_loop.remove_port(rx_addr);
-    UNSIGNED_LONGS_EQUAL(0, event_loop.num_ports());
 }
 
 } // namespace netio
