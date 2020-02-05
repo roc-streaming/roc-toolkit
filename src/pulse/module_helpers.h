fd802d3e4981321242577c9ffb4c255bf4dc7a82
Victor Gaydov <victor@enise.org>
2020-02-05 20:38:38 +0300

    Migrate API to interfaces and endpoints
    Issue: #258

--- src/pulse/module_helpers.h
+++ src/pulse/module_helpers.h
@@ -13,22 +13,27 @@
 #include <pulsecore/modargs.h>
 
 /* roc headers */
-#include <roc/address.h>
 #include <roc/config.h>
+#include <roc/endpoint.h>
 #include <roc/log.h>
 
 #define ROCPA_DEFAULT_IP "0.0.0.0"
+
 #define ROCPA_DEFAULT_SOURCE_PORT "10001"
 #define ROCPA_DEFAULT_REPAIR_PORT "10002"
 
+#define ROCPA_DEFAULT_SOURCE_PROTO ROC_PROTO_RTP_RS8M_SOURCE
+#define ROCPA_DEFAULT_REPAIR_PROTO ROC_PROTO_RS8M_REPAIR
+
 void rocpa_log_handler(roc_log_level level, const char* module, const char* message);
 
-int rocpa_parse_address(roc_address* addr,
-                        pa_modargs* args,
-                        const char* ip_arg,
-                        const char* default_ip_arg,
-                        const char* port_arg,
-                        const char* default_port_arg);
+int rocpa_parse_endpoint(roc_endpoint** endp,
+                         roc_protocol proto,
+                         pa_modargs* args,
+                         const char* ip_arg,
+                         const char* default_ip_arg,
+                         const char* port_arg,
+                         const char* default_port_arg);
 
 int rocpa_parse_duration_msec(unsigned long long* out,
                               unsigned long out_base,
