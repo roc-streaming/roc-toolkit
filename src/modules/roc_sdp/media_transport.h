2d783d9520fb12380b7b97a851b5dbe737b39d88
alexandremgo <alexandre.magaud@gmail.com>
2020-04-12 11:20:05 -0400

    Fixes from review

--- src/modules/roc_sdp/media_transport.h
+++ src/modules/roc_sdp/media_transport.h
@@ -6,25 +6,25 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-//! @file roc_sdp/media_proto.h
-//! @brief SDP media description transport protocol.
+//! @file roc_sdp/media_transport.h
+//! @brief SDP media transport protocol.
 
-#ifndef ROC_SDP_MEDIA_PROTO_H_
-#define ROC_SDP_MEDIA_PROTO_H_
+#ifndef ROC_SDP_MEDIA_TRANSPORT_H_
+#define ROC_SDP_MEDIA_TRANSPORT_H_
 
 namespace roc {
 namespace sdp {
 
-//! Address family.
-enum MediaProto {
-    //! Media proto is not set.
-    MediaProto_None,
+//! Media transport protocol.
+enum MediaTransport {
+    //! Media transport is not set.
+    MediaTransport_None,
 
     //! RTP/AVP.
-    MediaProto_RTP_AVP
+    MediaTransport_RTP_AVP
 };
 
 } // namespace sdp
 } // namespace roc
 
-#endif // ROC_SDP_MEDIA_PROTO_H_
+#endif // ROC_SDP_MEDIA_TRANSPORT_H_
