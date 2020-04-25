537ea0f05ee5aa975c22ef999ed3534d7a376f15
Victor Gaydov <victor@enise.org>
2020-04-25 20:17:38 +0300

    Add ctl::TaskQueue and ctl::ControlLoop
    Issues: #214, #215

---
+++ src/modules/roc_ctl/control_loop.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+//! @file roc_ctl/control_loop.h
+//! @brief Control loop thread.
+
+#ifndef ROC_CTL_CONTROL_LOOP_H_
+#define ROC_CTL_CONTROL_LOOP_H_
+
+#include "roc_ctl/task_queue.h"
+#include "roc_pipeline/task_pipeline.h"
+
+namespace roc {
+namespace ctl {
+
+//! Control loop thread.
+class ControlLoop : public TaskQueue {
+public:
+    //! Control loop task.
+    class Task : public TaskQueue::Task {
+    protected:
+        //! Initialize.
+        Task(TaskResult (ControlLoop::*func)(Task&));
+
+    private:
+        friend class ControlLoop;
+
+        TaskResult (ControlLoop::*func_)(Task&);
+    };
+
+    //! Subclasses for specific tasks.
+    class Tasks {
+    public:
+        //! Process pending pipeline tasks.
+        class ProcessPipelineTasks : public Task {
+        public:
+            //! Set task parameters.
+            ProcessPipelineTasks(pipeline::TaskPipeline& pipeline);
+
+        private:
+            friend class ControlLoop;
+
+            pipeline::TaskPipeline& pipeline_;
+        };
+    };
+
+    virtual ~ControlLoop();
+
+private:
+    virtual TaskResult process_task_imp(TaskQueue::Task&);
+
+    TaskResult task_process_pipeline_tasks_(Task&);
+};
+
+} // namespace ctl
+} // namespace roc
+
+#endif // ROC_CTL_CONTROL_LOOP_H_
---
+++ src/tests/roc_ctl/bench_task_queue_contention.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <benchmark/benchmark.h>
+
+#include "roc_core/fast_random.h"
+#include "roc_ctl/task_queue.h"
+
+namespace roc {
+namespace ctl {
+namespace {
+
+enum {
+    NumScheduleIterations = 100000,
+    NumScheduleAfterIterations = 10000,
+    NumThreads = 8,
+    BatchSize = 1000
+};
+
+const core::nanoseconds_t MaxDelay = 100 * core::Millisecond;
+
+class NoopQueue : public TaskQueue {
+public:
+    class Task : public TaskQueue::Task {
+    public:
+        Task() {
+        }
+    };
+
+    ~NoopQueue() {
+        stop_and_wait();
+    }
+
+    using TaskQueue::stop_and_wait;
+
+private:
+    virtual TaskResult process_task_imp(TaskQueue::Task&) {
+        return TaskSucceeded;
+    }
+};
+
+class NoopHandler : public TaskQueue::ICompletionHandler {
+public:
+    virtual void control_task_finished(TaskQueue::Task&) {
+    }
+};
+
+struct BM_QueueContention : benchmark::Fixture {
+    NoopQueue queue;
+    NoopHandler handler;
+};
+
+BENCHMARK_DEFINE_F(BM_QueueContention, Schedule)(benchmark::State& state) {
+    NoopQueue::Task* tasks = new NoopQueue::Task[NumScheduleIterations];
+    size_t n_task = 0;
+
+    while (state.KeepRunningBatch(BatchSize)) {
+        for (int n = 0; n < BatchSize; n++) {
+            queue.schedule(tasks[n_task++], &handler);
+        }
+    }
+
+    for (int n = 0; n < NumScheduleIterations; n++) {
+        queue.cancel_and_wait(tasks[n]);
+    }
+
+    delete[] tasks;
+}
+
+BENCHMARK_REGISTER_F(BM_QueueContention, Schedule)
+    ->ThreadRange(1, NumThreads)
+    ->Iterations(NumScheduleIterations)
+    ->Unit(benchmark::kMicrosecond);
+
+BENCHMARK_DEFINE_F(BM_QueueContention, ScheduleAfter)(benchmark::State& state) {
+    NoopQueue::Task* tasks = new NoopQueue::Task[NumScheduleAfterIterations];
+    size_t n_task = 0;
+
+    core::nanoseconds_t* delays = new core::nanoseconds_t[NumScheduleAfterIterations];
+    for (int n = 0; n < NumScheduleAfterIterations; n++) {
+        delays[n] = core::fast_random(0, MaxDelay);
+    }
+
+    while (state.KeepRunningBatch(BatchSize)) {
+        for (int n = 0; n < BatchSize; n++) {
+            queue.schedule_after(tasks[n_task], delays[n_task], &handler);
+            n_task++;
+        }
+    }
+
+    for (int n = 0; n < NumScheduleAfterIterations; n++) {
+        queue.cancel_and_wait(tasks[n]);
+    }
+
+    delete[] tasks;
+    delete[] delays;
+}
+
+BENCHMARK_REGISTER_F(BM_QueueContention, ScheduleAfter)
+    ->ThreadRange(1, NumThreads)
+    ->Iterations(NumScheduleAfterIterations)
+    ->Unit(benchmark::kMicrosecond);
+
+} // namespace
+} // namespace ctl
+} // namespace roc
