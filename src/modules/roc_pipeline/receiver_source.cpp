0bd1bc8a9bd84ecd3f17df052d65146249355e0c
Victor Gaydov <victor@enise.org>
2020-04-28 17:30:13 +0300

    Switch ReceiverSource to task-based API
    Issue: #215

--- src/modules/roc_pipeline/receiver_source.cpp
+++ src/modules/roc_pipeline/receiver_source.cpp
@@ -14,13 +14,69 @@
 namespace roc {
 namespace pipeline {
 
-ReceiverSource::ReceiverSource(const ReceiverConfig& config,
+ReceiverSource::Task::Task()
+    : func_(NULL)
+    , endpoint_set_(NULL)
+    , iface_(address::Iface_Invalid)
+    , proto_(address::Proto_None)
+    , writer_(NULL) {
+}
+
+ReceiverSource::Tasks::AddEndpointSet::AddEndpointSet() {
+    func_ = &ReceiverSource::task_add_endpoint_set_;
+}
+
+ReceiverSource::EndpointSetHandle
+ReceiverSource::Tasks::AddEndpointSet::get_handle() const {
+    if (!success()) {
+        return NULL;
+    }
+    roc_panic_if_not(endpoint_set_);
+    return (EndpointSetHandle)endpoint_set_;
+}
+
+ReceiverSource::Tasks::CreateEndpoint::CreateEndpoint(EndpointSetHandle endpoint_set,
+                                                      address::Interface iface,
+                                                      address::Protocol proto) {
+    func_ = &ReceiverSource::task_create_endpoint_;
+    if (!endpoint_set) {
+        roc_panic("receiver source: endpoint set handle is null");
+    }
+    endpoint_set_ = (ReceiverEndpointSet*)endpoint_set;
+    iface_ = iface;
+    proto_ = proto;
+}
+
+packet::IWriter* ReceiverSource::Tasks::CreateEndpoint::get_writer() const {
+    if (!success()) {
+        return NULL;
+    }
+    roc_panic_if_not(writer_);
+    return writer_;
+}
+
+ReceiverSource::Tasks::DeleteEndpoint::DeleteEndpoint(EndpointSetHandle endpoint_set,
+                                                      address::Interface iface) {
+    func_ = &ReceiverSource::task_delete_endpoint_;
+    if (!endpoint_set) {
+        roc_panic("receiver source: endpoint set handle is null");
+    }
+    endpoint_set_ = (ReceiverEndpointSet*)endpoint_set;
+    iface_ = iface;
+}
+
+ReceiverSource::ReceiverSource(ITaskScheduler& scheduler,
+                               const ReceiverConfig& config,
                                const rtp::FormatMap& format_map,
                                packet::PacketPool& packet_pool,
                                core::BufferPool<uint8_t>& byte_buffer_pool,
                                core::BufferPool<audio::sample_t>& sample_buffer_pool,
                                core::IAllocator& allocator)
-    : format_map_(format_map)
+    : TaskPipeline(scheduler,
+                   config.tasks,
+                   config.common.output_sample_rate,
+                   config.common.output_channels)
+    , format_map_(format_map)
     , packet_pool_(packet_pool)
     , byte_buffer_pool_(byte_buffer_pool)
     , sample_buffer_pool_(sample_buffer_pool)
@@ -54,50 +110,16 @@ bool ReceiverSource::valid() const {
     return audio_reader_;
 }
 
-ReceiverSource::EndpointSetHandle ReceiverSource::add_endpoint_set() {
-    core::Mutex::Lock lock(mutex_);
-
+bool ReceiverSource::read(audio::Frame& frame) {
     roc_panic_if(!valid());
 
-    roc_log(LogDebug, "receiver source: adding endpoint set");
+    core::Mutex::Lock lock(read_mutex_);
 
-    core::SharedPtr<ReceiverEndpointSet> endpoint_set = new (allocator_)
-        ReceiverEndpointSet(config_, receiver_state_, *mixer_, format_map_, packet_pool_,
-                            byte_buffer_pool_, sample_buffer_pool_, allocator_);
-
-    if (!endpoint_set) {
-        roc_log(LogError, "receiver source: can't allocate endpoint set");
-        return 0;
+    if (config_.common.timing) {
+        ticker_.wait(timestamp_);
     }
 
-    endpoint_sets_.push_back(*endpoint_set);
-
-    return (EndpointSetHandle)endpoint_set.get();
-}
-
-packet::IWriter* ReceiverSource::add_endpoint(EndpointSetHandle endpoint_set_handle,
-                                              address::Interface iface,
-                                              address::Protocol proto) {
-    core::Mutex::Lock lock(mutex_);
-
-    roc_panic_if(!valid());
-
-    ReceiverEndpointSet* endpoint_set = (ReceiverEndpointSet*)endpoint_set_handle;
-    roc_panic_if_not(endpoint_set);
-
-    return endpoint_set->add_endpoint(iface, proto);
-}
-
-void ReceiverSource::remove_endpoint(EndpointSetHandle endpoint_set_handle,
-                                     address::Interface iface) {
-    core::Mutex::Lock lock(mutex_);
-
-    roc_panic_if(!valid());
-
-    ReceiverEndpointSet* endpoint_set = (ReceiverEndpointSet*)endpoint_set_handle;
-    roc_panic_if_not(endpoint_set);
-
-    endpoint_set->remove_endpoint(iface);
+    return process_frame_and_tasks(frame);
 }
 
 size_t ReceiverSource::num_sessions() const {
@@ -129,7 +151,7 @@ sndio::ISource::State ReceiverSource::state() const {
         return Playing;
     }
 
-    // no sessions and packets; we can sleep
+    // no sessions and packets; we can sleep until there are some
     return Idle;
 }
 
@@ -145,15 +167,11 @@ bool ReceiverSource::restart() {
     return true;
 }
 
-bool ReceiverSource::read(audio::Frame& frame) {
-    core::Mutex::Lock lock(mutex_);
+core::nanoseconds_t ReceiverSource::timestamp_imp() const {
+    return core::timestamp();
+}
 
-    roc_panic_if(!valid());
-
-    if (config_.common.timing) {
-        ticker_.wait(timestamp_);
-    }
-
+bool ReceiverSource::process_frame_imp(audio::Frame& frame) {
     for (core::SharedPtr<ReceiverEndpointSet> endpoint_set = endpoint_sets_.front();
          endpoint_set; endpoint_set = endpoint_sets_.nextof(*endpoint_set)) {
         endpoint_set->update(timestamp_);
@@ -165,6 +183,43 @@ bool ReceiverSource::read(audio::Frame& frame) {
 
     timestamp_ += frame.size() / num_channels_;
 
+    return true;
+}
+
+bool ReceiverSource::process_task_imp(TaskPipeline::Task& basic_task) {
+    Task& task = (Task&)basic_task;
+
+    roc_panic_if_not(task.func_);
+
+    return (this->*(task.func_))(task);
+}
+
+bool ReceiverSource::task_add_endpoint_set_(Task& task) {
+    core::SharedPtr<ReceiverEndpointSet> endpoint_set = new (allocator_)
+        ReceiverEndpointSet(config_, receiver_state_, *mixer_, format_map_, packet_pool_,
+                            byte_buffer_pool_, sample_buffer_pool_, allocator_);
+    if (!endpoint_set) {
+        return false;
+    }
+
+    endpoint_sets_.push_back(*endpoint_set);
+    task.endpoint_set_ = endpoint_set.get();
+
+    return true;
+}
+
+bool ReceiverSource::task_create_endpoint_(Task& task) {
+    packet::IWriter* writer =
+        task.endpoint_set_->create_endpoint(task.iface_, task.proto_);
+    if (!writer) {
+        return false;
+    }
+    task.writer_ = writer;
+    return true;
+}
+
+bool ReceiverSource::task_delete_endpoint_(Task& task) {
+    task.endpoint_set_->delete_endpoint(task.iface_);
     return true;
 }
 
--- src/modules/roc_pipeline/receiver_source.h
+++ src/modules/roc_pipeline/receiver_source.h
@@ -17,9 +17,7 @@
 #include "roc_audio/poison_reader.h"
 #include "roc_core/buffer_pool.h"
 #include "roc_core/iallocator.h"
-#include "roc_core/list.h"
 #include "roc_core/mutex.h"
-#include "roc_core/noncopyable.h"
 #include "roc_core/scoped_ptr.h"
 #include "roc_core/stddefs.h"
 #include "roc_packet/ireader.h"
@@ -28,6 +26,7 @@
 #include "roc_pipeline/config.h"
 #include "roc_pipeline/receiver_endpoint_set.h"
 #include "roc_pipeline/receiver_state.h"
+#include "roc_pipeline/task_pipeline.h"
 #include "roc_rtp/format_map.h"
 #include "roc_sndio/isource.h"
 
@@ -39,10 +38,68 @@ namespace pipeline {
 //! @remarks
 //!  - input: packets
 //!  - output: frames
-class ReceiverSource : public sndio::ISource, public core::NonCopyable<> {
+class ReceiverSource : public sndio::ISource, public TaskPipeline {
 public:
+    //! Opaque endpoint set handle.
+    typedef struct EndpointSetHandle* EndpointSetHandle;
+
+    //! Base task class.
+    //! The user is responsible for allocating and deallocating the task.
+    class Task : public TaskPipeline::Task {
+    protected:
+        friend class ReceiverSource;
+
+        Task();
+
+        bool (ReceiverSource::*func_)(Task&); //!< Task implementation method.
+
+        ReceiverEndpointSet* endpoint_set_; //!< Endpoint set.
+        address::Interface iface_;          //!< Interface.
+        address::Protocol proto_;           //!< Protocol.
+        packet::IWriter* writer_;           //!< Packet writer.
+    };
+
+    //! Subclasses for specific tasks.
+    class Tasks {
+    public:
+        //! Add new endpoint set.
+        class AddEndpointSet : public Task {
+        public:
+            //! Set task parameters.
+            AddEndpointSet();
+
+            //! Get created endpoint set handle.
+            EndpointSetHandle get_handle() const;
+        };
+
+        //! Create endpoint on given interface of the endpoint set.
+        class CreateEndpoint : public Task {
+        public:
+            //! Set task parameters.
+            //! @remarks
+            //!  Each endpoint set can have one source and zero or one repair endpoint.
+            //!  The protocols of endpoints in one set should be compatible.
+            CreateEndpoint(EndpointSetHandle endpoint_set,
+                           address::Interface iface,
+                           address::Protocol proto);
+
+            //! Get packet writer for the endpoint.
+            //! @remarks
+            //!  The returned writer may be used from any thread.
+            packet::IWriter* get_writer() const;
+        };
+
+        //! Delete endpoint on given interface of the endpoint set, if it exists.
+        class DeleteEndpoint : public Task {
+        public:
+            //! Set task parameters.
+            DeleteEndpoint(EndpointSetHandle endpoint_set, address::Interface iface);
+        };
+    };
+
     //! Initialize.
-    ReceiverSource(const ReceiverConfig& config,
+    ReceiverSource(ITaskScheduler& scheduler,
+                   const ReceiverConfig& config,
                    const rtp::FormatMap& format_map,
                    packet::PacketPool& packet_pool,
                    core::BufferPool<uint8_t>& byte_buffer_pool,
@@ -51,26 +108,6 @@ public:
 
     //! Check if the pipeline was successfully constructed.
     bool valid() const;
-
-    //! Opaque endpoint set handle.
-    typedef struct EndpointSetHandle* EndpointSetHandle;
-
-    //! Add new endpoint set.
-    //! @returns
-    //!  NULL on on error or non-NULL opaque handle on success.
-    EndpointSetHandle add_endpoint_set();
-
-    //! Add endpoint to endpoint set.
-    //! Each endpoint set can have one source and zero or one repair endpoint.
-    //! The protocols of endpoints in one set should be compatible.
-    //! @returns
-    //!  thread-safe packet writer for the newly created endpoint or NULL on error.
-    packet::IWriter* add_endpoint(EndpointSetHandle endpoint_set,
-                                  address::Interface iface,
-                                  address::Protocol proto);
-
-    //! Remove endpoint to endpoint set, if it's present.
-    void remove_endpoint(EndpointSetHandle endpoint_set, address::Interface iface);
 
     //! Get number of connected sessions.
     size_t num_sessions() const;
@@ -97,10 +134,20 @@ public:
     virtual bool restart();
 
     //! Read frame.
+    //! May also process some tasks and invoke callbacks passed to enqueue().
     virtual bool read(audio::Frame&);
 
 private:
-    void update_(packet::timestamp_t timestamp);
+    virtual core::nanoseconds_t timestamp_imp() const;
+
+    virtual bool process_frame_imp(audio::Frame& frame);
+    virtual bool process_task_imp(TaskPipeline::Task& task);
+
+    bool task_add_endpoint_set_(Task& task);
+    bool task_create_endpoint_(Task& task);
+    bool task_delete_endpoint_(Task& task);
+
+    core::Mutex read_mutex_;
 
     const rtp::FormatMap& format_map_;
 
@@ -123,8 +170,6 @@ private:
 
     packet::timestamp_t timestamp_;
     size_t num_channels_;
-
-    core::Mutex mutex_;
 };
 
 } // namespace pipeline
