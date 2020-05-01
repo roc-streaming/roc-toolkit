d3ae33d2a06cf01b4acd21834c7397884c593e36
Victor Gaydov <victor@enise.org>
2020-05-01 20:12:34 +0300

    Switch SenderSink to task-based API
    Issue: #215

--- src/modules/roc_pipeline/sender_sink.cpp
+++ src/modules/roc_pipeline/sender_sink.cpp
@@ -14,13 +14,86 @@
 namespace roc {
 namespace pipeline {
 
-SenderSink::SenderSink(const SenderConfig& config,
+SenderSink::Task::Task()
+    : func_(NULL)
+    , endpoint_set_(NULL)
+    , endpoint_(NULL)
+    , iface_(address::Iface_Invalid)
+    , proto_(address::Proto_None)
+    , writer_(NULL) {
+}
+
+SenderSink::Tasks::AddEndpointSet::AddEndpointSet() {
+    func_ = &SenderSink::task_add_endpoint_set_;
+}
+
+SenderSink::EndpointSetHandle SenderSink::Tasks::AddEndpointSet::get_handle() const {
+    if (!success()) {
+        return NULL;
+    }
+    roc_panic_if_not(endpoint_set_);
+    return (EndpointSetHandle)endpoint_set_;
+}
+
+SenderSink::Tasks::CreateEndpoint::CreateEndpoint(EndpointSetHandle endpoint_set,
+                                                  address::Interface iface,
+                                                  address::Protocol proto) {
+    func_ = &SenderSink::task_create_endpoint_;
+    if (!endpoint_set) {
+        roc_panic("sender sink: endpoint set handle is null");
+    }
+    endpoint_set_ = (SenderEndpointSet*)endpoint_set;
+    iface_ = iface;
+    proto_ = proto;
+}
+
+SenderSink::EndpointHandle SenderSink::Tasks::CreateEndpoint::get_handle() const {
+    if (!success()) {
+        return NULL;
+    }
+    roc_panic_if_not(endpoint_);
+    return (EndpointHandle)endpoint_;
+}
+
+SenderSink::Tasks::SetEndpointOutputWriter::SetEndpointOutputWriter(
+    EndpointHandle endpoint, packet::IWriter& writer) {
+    func_ = &SenderSink::task_set_endpoint_output_writer_;
+    if (!endpoint) {
+        roc_panic("sender sink: endpoint handle is null");
+    }
+    endpoint_ = (SenderEndpoint*)endpoint;
+    writer_ = &writer;
+}
+
+SenderSink::Tasks::SetEndpointDestinationUdpAddress::SetEndpointDestinationUdpAddress(
+    EndpointHandle endpoint, const address::SocketAddr& addr) {
+    func_ = &SenderSink::task_set_endpoint_destination_udp_address_;
+    if (!endpoint) {
+        roc_panic("sender sink: endpoint handle is null");
+    }
+    endpoint_ = (SenderEndpoint*)endpoint;
+    addr_ = addr;
+}
+
+SenderSink::Tasks::CheckEndpointSetIsReady::CheckEndpointSetIsReady(
+    EndpointSetHandle endpoint_set) {
+    func_ = &SenderSink::task_check_endpoint_set_is_ready_;
+    if (!endpoint_set) {
+        roc_panic("sender sink: endpoint set handle is null");
+    }
+    endpoint_set_ = (SenderEndpointSet*)endpoint_set;
+}
+
+SenderSink::SenderSink(ITaskScheduler& scheduler,
+                       const SenderConfig& config,
                        const rtp::FormatMap& format_map,
                        packet::PacketPool& packet_pool,
                        core::BufferPool<uint8_t>& byte_buffer_pool,
                        core::BufferPool<audio::sample_t>& sample_buffer_pool,
                        core::IAllocator& allocator)
-    : config_(config)
+    : TaskPipeline(
+        scheduler, config.tasks, config.input_sample_rate, config.input_channels)
+    , config_(config)
     , format_map_(format_map)
     , packet_pool_(packet_pool)
     , byte_buffer_pool_(byte_buffer_pool)
@@ -55,86 +128,6 @@ bool SenderSink::valid() const {
     return audio_writer_;
 }
 
-SenderSink::EndpointSetHandle SenderSink::add_endpoint_set() {
-    core::Mutex::Lock lock(mutex_);
-
-    roc_panic_if(!valid());
-
-    roc_log(LogInfo, "sender sink: adding endpoint set");
-
-    core::SharedPtr<SenderEndpointSet> endpoint_set = new (allocator_)
-        SenderEndpointSet(config_, format_map_, packet_pool_, byte_buffer_pool_,
-                          sample_buffer_pool_, allocator_);
-
-    if (!endpoint_set) {
-        roc_log(LogError, "sender sink: can't allocate endpoint set");
-        return 0;
-    }
-
-    endpoint_sets_.push_back(*endpoint_set);
-
-    return (EndpointSetHandle)endpoint_set.get();
-}
-
-SenderSink::EndpointHandle SenderSink::add_endpoint(EndpointSetHandle endpoint_set_handle,
-                                                    address::Interface iface,
-                                                    address::Protocol proto) {
-    core::Mutex::Lock lock(mutex_);
-
-    roc_panic_if(!valid());
-
-    SenderEndpointSet* endpoint_set = (SenderEndpointSet*)endpoint_set_handle;
-    roc_panic_if_not(endpoint_set);
-
-    SenderEndpoint* endpoint = endpoint_set->add_endpoint(iface, proto);
-    if (!endpoint) {
-        return 0;
-    }
-
-    if (audio::IWriter* endpoint_set_writer = endpoint_set->writer()) {
-        if (!fanout_.has_output(*endpoint_set_writer)) {
-            fanout_.add_output(*endpoint_set_writer);
-        }
-    }
-
-    return (EndpointHandle)endpoint;
-}
-
-void SenderSink::set_endpoint_output_writer(EndpointHandle endpoint_handle,
-                                            packet::IWriter& writer) {
-    core::Mutex::Lock lock(mutex_);
-
-    roc_panic_if(!valid());
-
-    SenderEndpoint* endpoint = (SenderEndpoint*)endpoint_handle;
-    roc_panic_if_not(endpoint);
-
-    endpoint->set_output_writer(writer);
-}
-
-void SenderSink::set_endpoint_destination_udp_address(EndpointHandle endpoint_handle,
-                                                      const address::SocketAddr& addr) {
-    core::Mutex::Lock lock(mutex_);
-
-    roc_panic_if(!valid());
-
-    SenderEndpoint* endpoint = (SenderEndpoint*)endpoint_handle;
-    roc_panic_if_not(endpoint);
-
-    endpoint->set_destination_udp_address(addr);
-}
-
-bool SenderSink::is_endpoint_set_ready(EndpointSetHandle endpoint_set_handle) const {
-    core::Mutex::Lock lock(mutex_);
-
-    roc_panic_if(!valid());
-
-    SenderEndpointSet* endpoint_set = (SenderEndpointSet*)endpoint_set_handle;
-    roc_panic_if_not(endpoint_set);
-
-    return endpoint_set->is_ready();
-}
-
 size_t SenderSink::sample_rate() const {
     return config_.input_sample_rate;
 }
@@ -148,7 +141,7 @@ bool SenderSink::has_clock() const {
 }
 
 void SenderSink::write(audio::Frame& frame) {
-    core::Mutex::Lock lock(mutex_);
+    core::Mutex::Lock lock(write_mutex_);
 
     roc_panic_if(!valid());
 
@@ -156,9 +149,85 @@ void SenderSink::write(audio::Frame& frame) {
         ticker_->wait(timestamp_);
     }
 
+    process_frame_and_tasks(frame);
+}
+
+core::nanoseconds_t SenderSink::timestamp_imp() const {
+    return core::timestamp();
+}
+
+bool SenderSink::process_frame_imp(audio::Frame& frame) {
     audio_writer_->write(frame);
 
     timestamp_ += frame.size() / num_channels_;
+
+    return true;
+}
+
+bool SenderSink::process_task_imp(TaskPipeline::Task& basic_task) {
+    Task& task = (Task&)basic_task;
+
+    roc_panic_if_not(task.func_);
+
+    return (this->*(task.func_))(task);
+}
+
+bool SenderSink::task_add_endpoint_set_(Task& task) {
+    roc_log(LogInfo, "sender sink: adding endpoint set");
+
+    core::SharedPtr<SenderEndpointSet> endpoint_set = new (allocator_)
+        SenderEndpointSet(config_, format_map_, packet_pool_, byte_buffer_pool_,
+                          sample_buffer_pool_, allocator_);
+
+    if (!endpoint_set) {
+        roc_log(LogError, "sender sink: can't allocate endpoint set");
+        return false;
+    }
+
+    endpoint_sets_.push_back(*endpoint_set);
+
+    task.endpoint_set_ = endpoint_set.get();
+    return true;
+}
+
+bool SenderSink::task_create_endpoint_(Task& task) {
+    roc_panic_if(!task.endpoint_set_);
+
+    SenderEndpoint* endpoint =
+        task.endpoint_set_->create_endpoint(task.iface_, task.proto_);
+    if (!endpoint) {
+        return 0;
+    }
+
+    if (audio::IWriter* endpoint_set_writer = task.endpoint_set_->writer()) {
+        if (!fanout_.has_output(*endpoint_set_writer)) {
+            fanout_.add_output(*endpoint_set_writer);
+        }
+    }
+
+    task.endpoint_ = endpoint;
+    return true;
+}
+
+bool SenderSink::task_set_endpoint_output_writer_(Task& task) {
+    roc_panic_if(!task.endpoint_);
+    roc_panic_if(!task.writer_);
+
+    task.endpoint_->set_output_writer(*task.writer_);
+    return true;
+}
+
+bool SenderSink::task_set_endpoint_destination_udp_address_(Task& task) {
+    roc_panic_if(!task.endpoint_);
+
+    task.endpoint_->set_destination_udp_address(task.addr_);
+    return true;
+}
+
+bool SenderSink::task_check_endpoint_set_is_ready_(Task& task) {
+    roc_panic_if(!task.endpoint_set_);
+
+    return task.endpoint_set_->is_ready();
 }
 
 } // namespace pipeline
