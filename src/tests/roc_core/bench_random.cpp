cec9e27b454f77d655c5f157438554d7d3e7d2b7
Victor Gaydov <victor@enise.org>
2020-05-03 11:14:45 +0300

    Change ranged PRNG algorithm and add benchmark

---
+++ src/tests/roc_core/bench_random.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <benchmark/benchmark.h>
+
+#include "roc_core/fast_random.h"
+#include "roc_core/secure_random.h"
+
+namespace roc {
+namespace core {
+namespace {
+
+void Bench_FastRandom(benchmark::State& state) {
+    uint32_t r = 0;
+    while (state.KeepRunning()) {
+        r = fast_random(r, (uint32_t)-1);
+        benchmark::DoNotOptimize(r);
+    }
+}
+
+BENCHMARK(Bench_FastRandom);
+
+void Bench_SecureRandom(benchmark::State& state) {
+    uint32_t r = 0;
+    while (state.KeepRunning()) {
+        secure_random(r, (uint32_t)-1, r);
+        benchmark::DoNotOptimize(r);
+    }
+}
+
+BENCHMARK(Bench_SecureRandom);
+
+} // namespace
+} // namespace core
+} // namespace roc
