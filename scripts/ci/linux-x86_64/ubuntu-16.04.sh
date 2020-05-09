1d093056031162c77f74e22eab8f854373f03c3a
Victor Gaydov <victor@enise.org>
2020-05-09 09:36:56 +0300

    Increase minimum Google Benchmark version

--- scripts/ci/linux-x86_64/ubuntu-16.04.sh
+++ scripts/ci/linux-x86_64/ubuntu-16.04.sh
@@ -8,7 +8,7 @@ scons -Q \
       --enable-tests \
       --enable-benchmarks \
       --enable-examples \
-      --build-3rdparty=openfec,google-benchmark:1.2.0 \
+      --build-3rdparty=openfec,google-benchmark \
       test
 
 for c in gcc-4.8 gcc-5 clang-3.7
@@ -19,7 +19,7 @@ do
           --enable-tests \
           --enable-benchmarks \
           --enable-examples \
-          --build-3rdparty=openfec,pulseaudio,google-benchmark:1.2.0 \
+          --build-3rdparty=openfec,pulseaudio,google-benchmark \
           --compiler=$c \
           test
 done
