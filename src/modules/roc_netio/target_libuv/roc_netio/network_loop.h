c99df305aa5d763aa660ee91993df7343cfd6130
Victor Gaydov <victor@enise.org>
2020-05-13 18:56:15 +0300

    Use MpscQueue for packet queues
    Replace Mutex + List in UdpSenderPort and ReceiverEndpoint with a
    lock-free MpscQueue.
    
    This allows us to ensure that sender pipeline wont block on netio
    thread, and netio thread wont block on receiver pipeline.

--- src/modules/roc_netio/target_libuv/roc_netio/network_loop.h
+++ src/modules/roc_netio/target_libuv/roc_netio/network_loop.h
@@ -15,7 +15,7 @@
 #include <uv.h>
 
 #include "roc_address/socket_addr.h"
-#include "roc_core/atomic_int.h"
+#include "roc_core/atomic.h"
 #include "roc_core/buffer_pool.h"
 #include "roc_core/cond.h"
 #include "roc_core/iallocator.h"
@@ -65,12 +65,12 @@ public:
 
         //! Task state, defines whether task is finished already.
         //! The task becomes immutable after setting state to Finished.
-        core::AtomicInt state_;
+        core::Atomic<int> state_;
 
         //! Task result, defines wether finished task succeeded or failed.
         //! Makes sense only after setting state_ to Finished.
         //! This atomic should be assigned before setting state_ to Finished.
-        core::AtomicInt success_;
+        core::Atomic<int> success_;
 
         core::SharedPtr<BasicPort> port_; //!< On which port the task operates.
 
@@ -242,7 +242,7 @@ private:
     core::List<BasicPort> open_ports_;
     core::List<BasicPort> closing_ports_;
 
-    core::AtomicInt num_open_ports_;
+    core::Atomic<int> num_open_ports_;
 };
 
 } // namespace netio
