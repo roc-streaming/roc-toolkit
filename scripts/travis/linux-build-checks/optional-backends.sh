b7e8bf931b3ec4e9e8dbf389583d31a7fadfa75b
Victor Gaydov <victor@enise.org>
2020-05-28 11:47:24 +0300

    Add speexdsp to travis, sphinx, and vendor

--- scripts/travis/linux-build-checks/optional-backends.sh
+++ scripts/travis/linux-build-checks/optional-backends.sh
@@ -4,28 +4,29 @@ set -euxo pipefail
 scons -Q clean
 
 scons -Q --enable-werror --build-3rdparty=all \
-      --disable-lib \
-      --disable-tools \
-      --disable-doc \
-      --disable-c11 \
-      --disable-libunwind \
-      --disable-openfec \
-      --disable-speex \
+      --disable-pulseaudio \
       --disable-sox \
-      --disable-pulseaudio
-
-scons -Q --enable-werror --build-3rdparty=all \
       --enable-tests \
       --enable-benchmarks \
       --enable-examples \
-      --disable-libunwind \
-      --disable-openfec \
-      --disable-speex \
-      --disable-sox \
-      --disable-pulseaudio \
       test
 
 scons -Q --enable-werror --build-3rdparty=all \
+      --disable-pulseaudio \
+      --enable-tests \
+      --enable-benchmarks \
+      --enable-examples \
+      test
+
+scons -Q --enable-werror --build-3rdparty=all \
+      --disable-sox \
+      --enable-tests \
+      --enable-benchmarks \
+      --enable-examples \
+      test
+
+scons -Q --enable-werror --build-3rdparty=all \
+      --enable-pulseaudio-modules \
       --enable-tests \
       --enable-benchmarks \
       --enable-examples \
