973e614db45ec86acdca65d6a866a9fc9bbfc997
Victor Gaydov <victor@enise.org>
2020-05-28 14:28:44 +0300

    Fix warnings in cpu_traits

--- src/modules/roc_core/cpu_traits.h
+++ src/modules/roc_core/cpu_traits.h
@@ -14,20 +14,73 @@
 
 #include "roc_core/stddefs.h"
 
-#if UINTPTR_MAX == 0xffffffffffffffff || __UINTPTR_MAX__ == 0xffffffffffffffff           \
-    || SIZE_MAX == 0xffffffffffffffff || __SIZE_MAX__ == 0xffffffffffffffff              \
-    || __SIZEOF_POINTER__ == 8 || __SIZEOF_SIZE_T__ == 8 || __LP64__
+// On some environments, some standard macros like UINT_MAX or SIZE_MAX are
+// defined to expressions that can't be evaluated at preprocessing time and a
+// simple check like SIZE_MAX == 0xffffffffffffffff may not always work. Thus,
+// we do our best and check many different standard and non-standard macros.
+//
+// We avoid comparisons with other macros like SIZE_MAX == UINT_MAX, since if
+// both are defined to something that can not be evaluated by preprocessor,
+// both will be assumed to be zeros and the check may incorrectly pass.
+
+#ifndef ROC_CPU_64BIT
+#ifdef UINTPTR_MAX
+#if UINTPTR_MAX == 0xffffffffffffffff
 #define ROC_CPU_64BIT 1
-#else
+#endif
+#endif
+#endif
+
+#ifndef ROC_CPU_64BIT
+#ifdef __UINTPTR_MAX__
+#if __UINTPTR_MAX__ == 0xffffffffffffffff
+#define ROC_CPU_64BIT 1
+#endif
+#endif
+#endif
+
+#ifndef ROC_CPU_64BIT
+#ifdef SIZE_MAX
+#if SIZE_MAX == 0xffffffffffffffff
+#define ROC_CPU_64BIT 1
+#endif
+#endif
+#endif
+
+#ifndef ROC_CPU_64BIT
+#ifdef __SIZE_MAX__
+#if __SIZE_MAX__ == 0xffffffffffffffff
+#define ROC_CPU_64BIT 1
+#endif
+#endif
+#endif
+
+#ifndef ROC_CPU_64BIT
+#ifdef __SIZEOF_POINTER__
+#if __SIZEOF_POINTER__ == 8
+#define ROC_CPU_64BIT 1
+#endif
+#endif
+#endif
+
+#ifndef ROC_CPU_64BIT
+#ifdef __SIZEOF_SIZE_T__
+#if __SIZEOF_SIZE_T__ == 8
+#define ROC_CPU_64BIT 1
+#endif
+#endif
+#endif
+
+#ifndef ROC_CPU_64BIT
+#ifdef __LP64__
+#if __LP64__
+#define ROC_CPU_64BIT 1
+#endif
+#endif
+#endif
+
+#ifndef ROC_CPU_64BIT
 //! Whether we target 64-bit CPUs.
-//! @remarks
-//!  - On some environments, some standard macros like UINT_MAX or SIZE_MAX are
-//!    defined to expressions that can't be evaluated by preprocessor and a simple
-//!    check like SIZE_MAX == 0xffffffffffffffff may not always work. Thus, we
-//!    do our best and check many different standard and non-standard macros.
-//!  - We avoid comparisons with other macros like SIZE_MAX == UINT_MAX, since if
-//!    both are defined to something that can not be evaluated by preprocessor,
-//!    both will be assumed to be zeros and the check may incorrectly pass.
 #define ROC_CPU_64BIT 0
 #endif
 
