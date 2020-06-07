de61ad008f46b473b51a4fb2602d68d8d1f61bb5
Miguel Victoria <miguelvicvil@gmail.com>
2020-06-07 12:03:11 +0200

    Cleaning ubuntu scripts

--- scripts/ci/linux-x86_64/ubuntu-16.04.sh
+++ scripts/ci/linux-x86_64/ubuntu-16.04.sh
@@ -3,15 +3,7 @@ set -euxo pipefail
 
 scons -Q clean
 
-scons -Q \
-      --enable-werror \
-      --enable-tests \
-      --enable-benchmarks \
-      --enable-examples \
-      --build-3rdparty=openfec,google-benchmark \
-      test
-
-for c in gcc-4.8 gcc-5 clang-3.7
+for c in gcc-4.8 clang-3.7
 do
     scons -Q \
           --enable-werror \
