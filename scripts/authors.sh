b3c57c64b8e428193c74504be616986f85cc767e
Victor Gaydov <victor@enise.org>
2020-07-08 14:08:33 +0300

    Update authors

--- scripts/authors.sh
+++ scripts/authors.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+function find_login() {
+    curl -s "https://api.github.com/search/users?q=$1" \
+        | jq -r '.items[0].login' 2>/dev/null
+}
+
 function find_name() {
-    github_name="$(curl -s "https://api.github.com/search/users?q=$1" \
-        | jq -r .items[0].login 2>/dev/null \
-        | xargs -i curl -s "https://api.github.com/users/{}" \
+    local github_name="$(curl -s "https://api.github.com/users/$1" \
         | jq -r .name 2>/dev/null \
         | sed -r -e 's,^\s*,,' -e 's,\s*$,,')"
 
@@ -13,33 +16,55 @@ function find_name() {
     fi
 }
 
+function find_email() {
+    local github_email="$(curl -s "https://api.github.com/users/$1/events/public" \
+        | jq -r \
+    '((.[].payload.commits | select(. != null))[].author | select(.name == "'$1'")).email' \
+        | sort -u \
+        | grep -v users.noreply.github.com \
+        | head -1)"
+
+    if [[ "${github_email}" != "" ]] && [[ "${github_email}" != "null" ]]
+    then
+        echo "${github_email}"
+    fi
+}
+
 function add_if_new() {
-    line="$1"
-    file="$2"
+    local file="$1"
 
-    name="$(echo "${line}" | sed -re 's,^(.*?)\s+<[^>]+>$,\1,')"
-    email="$(echo "${line}" | sed -re 's,^.*<([^>]+)>$,\1,')"
+    local commit_name="$2"
+    local commit_email="$3"
 
-    if grep -qiF "${name}" "${file}" || grep -qiF "${email}" "${file}"
+    if grep -qiF "${commit_name}" "${file}" || grep -qiF "${commit_email}" "${file}"
     then
         return
     fi
 
-    echo "adding ${email}" 1>&2
-
-    print_name="$(find_name "${email}")"
-
-    if [ -z "${print_name}" ]
+    local github_login="$(find_login "${commit_email}")"
+    if [ -z "${login}" ]
     then
-        print_name="$(find_name "${name}")"
+        github_login="$(find_login "${commit_name}")"
     fi
 
+    local print_name="$(find_name "${github_login}")"
     if [ -z "${print_name}" ]
     then
-        print_name="${name}"
+        print_name="${commit_name}"
     fi
 
-    echo "* ${print_name} <${email}>"
+    local print_email=""
+    if echo "${commit_email}" | grep -q users.noreply.github.com
+    then
+        print_email="$(find_email "${github_login}")"
+    fi
+    if [ -z "${print_email}" ]
+    then
+        print_email="${commit_email}"
+    fi
+
+    echo "adding ${print_name} <${print_email}>" 1>&2
+    echo "* ${print_name} <${print_email}>"
 }
 
 function add_contributors() {
@@ -51,13 +76,17 @@ function add_contributors() {
         return
     fi
 
-    GIT_DIR="${repo_dir}"/.git git shortlog --summary --numbered --email \
-        | sed -r -e 's,^\s*,,' -e 's,\s+, ,g' \
-        | cut -d' ' -f2- \
+    GIT_DIR="${repo_dir}"/.git \
+           git log --encoding=utf-8 --full-history --reverse "--format=format:%at,%an,%ae" \
+        | sort -u -t, -k3,3 \
+        | sort -t, -k1n \
         | while read line
-        do
-            add_if_new "${line}" "${out_file}" >> "${out_file}"
-        done
+    do
+        name="$(echo "${line}" | cut -d, -f2)"
+        email="$(echo "${line}" | cut -d, -f3)"
+
+        add_if_new "${out_file}" "${name}" "${email}" >> "${out_file}"
+    done
 }
 
 cd "$(dirname "$0")/.."
@@ -67,13 +96,13 @@ temp="$(mktemp)"
 
 cat "$file" > "$temp"
 
-add_contributors "${temp}" "roc"
-add_contributors "${temp}" "roc-tests"
+add_contributors "${temp}" "roc-toolkit"
 add_contributors "${temp}" "roc-go"
 add_contributors "${temp}" "roc-java"
-add_contributors "${temp}" "roc/vendor"
+add_contributors "${temp}" "rt-tests"
 add_contributors "${temp}" "openfec"
 add_contributors "${temp}" "dockerfiles"
+add_contributors "${temp}" "roc-toolkit/vendor"
 
 cat "$temp" > "$file"
 rm "$temp"
