cf7669706fcb9e4207faa5b897524c243f4b8327
Arella Matteo <arella.matteo.95@gmail.com>
2020-07-12 16:43:30 +0200

    removed Android API 26 from testing

--- scripts/travis/android/i686-linux-android.sh
+++ scripts/travis/android/i686-linux-android.sh
@@ -10,4 +10,4 @@ scons -Q \
     --enable-benchmarks \
     --build-3rdparty=libuv,openfec,speexdsp,cpputest,google-benchmark \
     --compiler=clang \
-    --host=i686-linux-android$ANDROID_API
+    --host=i686-linux-android$API
