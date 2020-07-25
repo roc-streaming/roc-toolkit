b44247b47bd8f6541c285655bee84c54fac8d9cd
Arella Matteo <arella.matteo.95@gmail.com>
2020-07-25 17:11:07 +0200

    android tests on emulator

--- scripts/travis/android/x86_64-linux-android.sh
+++ scripts/travis/android/x86_64-linux-android.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 set -euxo pipefail
 
+target_prefix="/data/local/tmp"
+
 scons -Q clean
 
 scons -Q \
@@ -11,3 +13,21 @@ scons -Q \
     --build-3rdparty=libuv,openfec,speexdsp,cpputest,google-benchmark \
     --compiler=clang \
     --host=x86_64-linux-android$API
+
+device create --name roc-device-x86_64-linux-android$API --image default --api $API --arch x86_64
+device start --name roc-device-x86_64-linux-android$API
+
+adb shell "su 0 mkdir -p ${target_prefix}/lib"
+adb push "$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/x86_64-linux-android/libc++_shared.so" \
+        $target_prefix/lib/libc++_shared.so 1> /dev/null
+
+readarray -d '' tests < <(find bin/x86_64-linux-android$API \
+        -name 'roc-test-*' -not -name 'roc-test-library' -print0)
+
+for t in "${tests[@]}"; do
+    filename=$(basename "${t}")
+    adb push $t $target_prefix/$filename 1> /dev/null
+    python2 scripts/wrappers/timeout.py 300 \
+        adb shell "LD_LIBRARY_PATH=${target_prefix}/lib" \
+            "${target_prefix}/${filename}"
+done
