bf57eab309c12325be25b381309412c90d98d2a4
vimal patel <pvimal816@gmail.com>
2020-08-05 18:35:22 +0530

    Enable backtrace for undefined behavior sanitizer by default

---
+++ src/sanitizer_options.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Roc authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "roc_core/attributes.h"
+
+extern "C" ROC_ATTR_NO_SANITIZE_UB ROC_ATTR_EXPORT const char* __ubsan_default_options();
+
+extern "C" ROC_ATTR_NO_SANITIZE_UB ROC_ATTR_EXPORT const char* __ubsan_default_options() {
+    return "print_stacktrace=1";
+}
