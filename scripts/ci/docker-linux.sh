42855de6933d30da6e4b73ebece074011dbe88c4
Victor Gaydov <victor@enise.org>
2021-02-22 19:03:31 +0300

    scons: Dump log on error on CI

--- scripts/ci/docker-linux.sh
+++ scripts/ci/docker-linux.sh
@@ -4,4 +4,6 @@ docker run --rm -t \
        --cap-add SYS_PTRACE \
        -u "${UID}" \
        -v "$(pwd)":/opt/roc \
-       -w /opt/roc "$@"
+       -w /opt/roc \
+       -e CI="${CI}" \
+       "$@"
