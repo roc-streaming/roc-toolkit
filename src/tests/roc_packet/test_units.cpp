763298ee4f2d7f7e2632630d012c7dd8e7f60b11
Victor Gaydov <victor@enise.org>
2021-06-24 16:41:41 +0300

    Workaround for clang-format issue
    Issue: #378

--- src/tests/roc_packet/test_units.cpp
+++ src/tests/roc_packet/test_units.cpp
@@ -13,16 +13,21 @@
 namespace roc {
 namespace packet {
 
-TEST_GROUP(units) {
-    inline size_t num_channels(packet::channel_mask_t ch_mask) { size_t n_ch = 0;
-for (; ch_mask != 0; ch_mask >>= 1) {
-    if (ch_mask & 1) {
-        n_ch++;
+namespace {
+
+inline size_t num_channels(packet::channel_mask_t ch_mask) {
+    size_t n_ch = 0;
+    for (; ch_mask != 0; ch_mask >>= 1) {
+        if (ch_mask & 1) {
+            n_ch++;
+        }
     }
+    return n_ch;
 }
-return n_ch;
-}
-};
+
+} // namespace
+
+TEST_GROUP(units) {};
 
 TEST(units, seqnum_diff) {
     const seqnum_t v = 65535;
