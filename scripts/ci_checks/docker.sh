7c648c8643c8efd79daa6be5f85aedeca1015fa7
Victor Gaydov <victor@enise.org>
2022-12-01 13:02:51 +0400

    Enable ^C when run ci build locally

--- scripts/ci_checks/docker.sh
+++ scripts/ci_checks/docker.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-set -ex
-docker run --rm -t \
+set -euxo pipefail
+
+if [ -z "${CI:-}" ]; then
+    opts=-ti
+else
+    opts=-t
+fi
+
+docker run --rm ${opts} \
        --cap-add SYS_PTRACE \
        -u "${UID}" \
        -v "$(pwd)":/opt/roc \
        -w /opt/roc \
-       -e CI="${CI}" \
+       -e CI="${CI:-}" \
        "$@"
