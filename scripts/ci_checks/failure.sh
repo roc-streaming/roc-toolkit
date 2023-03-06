50321db500571b7e26557920ec1f35ef47e6a189
Victor Gaydov <victor@enise.org>
2023-03-06 13:38:50 +0400

    Improve failure script

--- scripts/ci_checks/failure.sh
+++ scripts/ci_checks/failure.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -e config.log ]; then
-    cat config.log
-fi
+env
+
+cat config.log || true
 
 if [ -d .sconf_temp ]; then
     for f in .sconf_temp/*.c; do
         echo
         echo $f
-        cat $f
+        cat $f || true
     done
 fi
