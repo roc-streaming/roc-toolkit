d62b4b747339bf9b36fde6333f0d3b3f52961ad2
Dheeraj Devaraj <dheeraj020602@gmail.com>
2023-09-01 09:19:53 +0530

    Cleanup commit
    Co-authored-by: Andrew <4029800+adrianopol@users.noreply.github.com>
    
    Code Review changes
    
    Clean up commit

--- scripts/ci_checks/linux-checks/check-version.sh
+++ scripts/ci_checks/linux-checks/check-version.sh
@@ -3,18 +3,19 @@
 set -euo pipefail
 
 
-tagname=$(git tag -l)
-tagname=${tagname: -5}
-header_tagname=$(python3 scripts/scons_helpers/parse-version.py)
+tagname="$(git tag -l | tail -n1)"
+
+header_tagname="v$(python3 scripts/scons_helpers/parse-version.py)"
 
 if [[ "$tagname" != "$header_tagname" ]]; then
-      echo
-      echo "version check FAILED"
-      echo "the git tag name and version declared in version.h is not the same"
-      echo "${tagname} git version tag"
-      echo "${header_tagname} header version tag"
-      echo
-      exit 1
-  else
-     echo "version check OK"
+  echo
+  echo "version check FAILED"
+  echo "the git tag name and version declared in version.h is not the same"
+  echo "${tagname} git version tag"
+  echo "${header_tagname} header version tag"
+  echo -e "\n"
+  exit 1
 fi
+
+echo "version check SUCCEEDED"
+echo -e "\n"
