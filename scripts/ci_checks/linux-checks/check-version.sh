e1818b59763a63e04f80413bc13778a3d30e8f69
Victor Gaydov <victor@enise.org>
2023-09-21 13:32:26 +0400

    Fixes in release checks workflow

--- scripts/ci_checks/linux-checks/check-version.sh
+++ scripts/ci_checks/linux-checks/check-version.sh
@@ -2,21 +2,19 @@
 
 set -euo pipefail
 
-
 tagname="$(git tag -l | tail -n1)"
 
 header_tagname="v$(python3 scripts/scons_helpers/parse-version.py)"
 
 if [[ "$tagname" != "$header_tagname" ]]; then
-  echo >&2 
-  echo >&2"version check FAILED"
-  echo >&2"the git tag name and version declared in version.h is not the same"
-  echo >&2"${tagname} git version tag"
-  echo >&2"${header_tagname} header version tag"
-  echo >&2
+  echo
+  echo "version check FAILED"
+  echo "the git tag name and version declared in version.h is not the same"
+  echo "${tagname} git version tag"
+  echo "${header_tagname} header version tag"
+  echo
   exit 1
 fi
 
 echo "version check SUCCEEDED"
-echo 
-
+echo
