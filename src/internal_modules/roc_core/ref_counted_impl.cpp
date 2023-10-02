85392dad6b88e018237341de07762a0a76f5c1bc
nolan-veed <88709630+nolan-veed@users.noreply.github.com>
2023-10-02 16:21:11 +0100

    Extacted non-templated code out of RefCounted.

--- src/internal_modules/roc_core/ref_counted_impl.cpp
+++ src/internal_modules/roc_core/ref_counted_impl.cpp
@@ -1,3 +1,70 @@
-//
-// Created by nolan on 01/10/23.
-//
+/*
+ * Copyright (c) 2023 Roc Streaming authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+//! @file roc_core/ref_counted_impl.cpp
+//! @brief Implementation class for object with reference counter.
+
+#include "roc_core/ref_counted_impl.h"
+#include "roc_core/panic.h"
+
+namespace roc {
+namespace core {
+
+RefCountedImpl::RefCountedImpl()
+    : counter_(0) {
+}
+
+RefCountedImpl::~RefCountedImpl() {
+    if (!counter_.compare_exchange(0, -1)) {
+        roc_panic("ref counter:"
+                  " attempt to destroy object that is in use, destroyed, or corrupted:"
+                  " counter=%d",
+                  (int)counter_);
+    }
+}
+
+int RefCountedImpl::getref() const {
+    const int current_counter = counter_;
+
+    if (current_counter < 0 || current_counter > MaxCounter) {
+        roc_panic("ref counter:"
+                  " attempt to access destroyed or currupted object:"
+                  " counter=%d",
+                  (int)current_counter);
+    }
+
+    return current_counter;
+}
+
+int RefCountedImpl::incref() const {
+    const int current_counter = ++counter_;
+
+    if (current_counter < 0 || current_counter > MaxCounter) {
+        roc_panic("ref counter:"
+                  " attempt to access destroyed or currupted object"
+                  " counter=%d",
+                  (int)current_counter);
+    }
+
+    return current_counter;
+}
+
+int RefCountedImpl::decref() const {
+    const int current_counter = --counter_;
+
+    if (current_counter < 0 || current_counter > MaxCounter) {
+        roc_panic("ref counter:"
+                  " attempt to access destroyed or currupted object"
+                  " counter=%d",
+                  (int)current_counter);
+    }
+    return current_counter;
+}
+
+} // namespace core
+} // namespace roc
--- src/internal_modules/roc_core/ref_counted_impl.h
+++ src/internal_modules/roc_core/ref_counted_impl.h
@@ -1,10 +1,10 @@
 /*
-* Copyright (c) 2023 Roc Streaming authors
-*
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
+ * Copyright (c) 2023 Roc Streaming authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 //! @file roc_core/ref_counted_impl.h
 //! @brief Implementation class for object with reference counter.
@@ -12,13 +12,33 @@
 #ifndef ROC_CORE_REF_COUNTED_IMPL_H_
 #define ROC_CORE_REF_COUNTED_IMPL_H_
 
+#include "roc_core/atomic.h"
 
 namespace roc {
 namespace core {
 
-class RefCountedImpl
-{
+class RefCountedImpl {
+public:
+    //! Initialize.
+    RefCountedImpl();
 
+    ~RefCountedImpl();
+
+    //! Get reference counter.
+    int getref() const;
+
+    //! Increment reference counter.
+    //! @returns counter value after incrementing.
+    int incref() const;
+
+    //! Decrement reference counter.
+    //! @returns counter value after decrementing.
+    int decref() const;
+
+private:
+    enum { MaxCounter = 100000 };
+
+    mutable Atomic<int> counter_;
 };
 
 } // namespace core
