a40e5c559c5f9909b216c5d7fee3b0bf9071b7ff
Victor Gaydov <victor@enise.org>
2023-10-06 02:00:50 +0400

    Plotting scripts

---
+++ scripts/plot/plot_csv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import fileinput
+import numpy
+import pylab
+import sys
+
+text = []
+columns = []
+
+for line in fileinput.input(encoding='utf-8'):
+    line = line.strip()
+    if line.startswith('#'):
+        columns = line.replace('#', '').split(',')
+        continue
+    text.append(line)
+
+data = numpy.genfromtxt(text, dtype=float, delimiter=',')
+
+if not columns:
+    columns = [None] * data.shape[1]
+
+fig, axs = pylab.subplots(len(columns), 1, sharex=True)
+
+if len(columns) == 1:
+    axs = [axs]
+
+for col_idx, col_name in enumerate(columns):
+    axs[col_idx].ticklabel_format(style='plain')
+    if col_name:
+        axs[col_idx].set_title(col_name)
+    axs[col_idx].grid()
+    axs[col_idx].plot(data[:,col_idx], '-*')
+
+pylab.show()
