f41392e71dd4f6c50598d31582bbe451f9046ac0
ForeverASilver <77247305+ForeverASilver@users.noreply.github.com>
2023-10-06 07:28:49 +0000

    Implement lock-free fast_random function

--- src/internal_modules/roc_core/target_posix/roc_core/fast_random.cpp
+++ src/internal_modules/roc_core/target_posix/roc_core/fast_random.cpp
@@ -9,6 +9,7 @@
 #include <pthread.h>
 #include <stdlib.h>
 
+#include "roc_core/atomic_ops.h"
 #include "roc_core/errno_to_str.h"
 #include "roc_core/fast_random.h"
 #include "roc_core/panic.h"
@@ -19,20 +20,32 @@ namespace core {
 
 namespace {
 
-pthread_mutex_t rand_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_once_t once_control = PTHREAD_ONCE_INIT;
 
-bool rand_init = false;
+uint32_t state;
 
-unsigned short rand_seed[3] = {};
+void init_state() {
+    const nanoseconds_t seed_48 = timestamp(ClockMonotonic);
+    state = (uint32_t)seed_48;
+}
+
+inline uint32_t splitmix32(uint32_t z) {
+    z = z ^ (z >> 16);
+    z *= 0x21F0AAAD;
+    z = z ^ (z >> 15);
+    z *= 0x735A2D97;
+    z = z ^ (z >> 15);
+    return z;
+}
 
 } // namespace
 
 // The implementation is based on "Debiased Modulo (Once) — Java's Method" algorithm
 // from https://www.pcg-random.org/posts/bounded-rands.html
 //
-// We use nrand48(), which is thread-safe on most platforms. It is probably not fully
-// thread-safe on glibc when used concurrently with lcong48(), but most likely the
-// race is harmless. See https://www.evanjones.ca/random-thread-safe.html.
+// We use splitmix32 as a PRNG
+// shifts and multiplcation value were taken from link below
+// https://gist.github.com/tommyettinger/46a874533244883189143505d203312c?permalink_comment_id=4365431#gistcomment-4365431
 //
 // This implementation is not a cryptographically secure PRNG.
 uint32_t fast_random(uint32_t from, uint32_t to) {
@@ -40,28 +53,17 @@ uint32_t fast_random(uint32_t from, uint32_t to) {
 
     const uint64_t range = uint64_t(to) - from + 1;
 
-    uint64_t x, r;
+    uint64_t z, r;
 
-    if (int err = pthread_mutex_lock(&rand_mutex)) {
-        roc_panic("fast random: pthread_mutex_lock(): %s", errno_to_str(err).c_str());
-    }
-
-    if (!rand_init) {
-        rand_init = true;
-        const nanoseconds_t seed_48 = timestamp(ClockMonotonic);
-        rand_seed[0] = (seed_48 & 0xffff);
-        rand_seed[1] = ((seed_48 >> 16) & 0xffff);
-        rand_seed[2] = ((seed_48 >> 32) & 0xffff);
+    if (int err = pthread_once(&once_control, init_state)) {
+        roc_panic("fast random: pthread_once(): %s", errno_to_str(err).c_str());
     }
 
     do {
-        x = (uint64_t)nrand48(rand_seed);
-        r = x % range;
-    } while (x - r > (-range));
-
-    if (int err = pthread_mutex_unlock(&rand_mutex)) {
-        roc_panic("fast random: pthread_mutex_unlock(): %s", errno_to_str(err).c_str());
-    }
+        z = AtomicOps::fetch_add_seq_cst(state, 0x9E3779B9);
+        z = splitmix32(z);
+        r = z % range;
+    } while (z - r > (-range));
 
     const uint32_t ret = from + (uint32_t)r;
 
