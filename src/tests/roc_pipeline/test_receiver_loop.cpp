80d32e4289a206f58ad26c08465dfb1ae4812d8b
Victor Gaydov <victor@enise.org>
2023-11-06 20:20:19 +0400

    Rename in test

--- src/tests/roc_pipeline/test_receiver_loop.cpp
+++ src/tests/roc_pipeline/test_receiver_loop.cpp
@@ -8,7 +8,7 @@
 
 #include <CppUTest/TestHarness.h>
 
-#include "test_helpers/scheduler.h"
+#include "test_helpers/mock_scheduler.h"
 
 #include "roc_core/buffer_factory.h"
 #include "roc_core/heap_arena.h"
@@ -100,7 +100,7 @@ private:
 } // namespace
 
 TEST_GROUP(receiver_loop) {
-    test::Scheduler scheduler;
+    test::MockScheduler scheduler;
 
     ReceiverConfig config;
 
