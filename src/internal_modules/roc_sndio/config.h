7581bf203b5fe2a1b89875cc0873125ccee9e681
Victor Gaydov <victor@enise.org>
2023-11-21 21:57:33 +0400

    Change default I/O frame length
    Increase frame len to 10ms to avoid underruns on cheap CPUs/cards.

--- src/internal_modules/roc_sndio/config.h
+++ src/internal_modules/roc_sndio/config.h
@@ -21,7 +21,9 @@ namespace roc {
 namespace sndio {
 
 //! Default frame length.
-const core::nanoseconds_t DefaultFrameLength = 1 * core::Millisecond;
+//!  10ms is rather high, but works well even on cheap sound cards and CPUs.
+//!  Usually you can use much lower values.
+const core::nanoseconds_t DefaultFrameLength = 10 * core::Millisecond;
 
 //! Sink and source config.
 struct Config {
