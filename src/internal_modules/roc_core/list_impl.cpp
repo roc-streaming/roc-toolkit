5339306ec2f4a8845006ddb226d1d3482510b201
Veronika Rovnik <veronikarovnik@gmail.com>
2024-03-03 14:51:04 +0100

    Add forgotten registering of list in ListNodeData

--- src/internal_modules/roc_core/list_impl.cpp
+++ src/internal_modules/roc_core/list_impl.cpp
@@ -98,7 +98,7 @@ void ListImpl::remove(ListNode::ListNodeData* data) {
 }
 
 void ListImpl::check_is_member(const ListNode::ListNodeData* data, const ListImpl* list) {
-    if (!list->contains(data)) {
+    if (data->list != list) {
         roc_panic(
             "list: list node data is not registered in this list: expected %p, got %p",
             (const void*)list, (const void*)data->list);
