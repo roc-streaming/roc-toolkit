f624da0ebe13533940ff9af78456552ee0eb5ee8
Victor Gaydov <victor@enise.org>
2024-05-05 16:54:14 +0400

    ci: Disable emulator on android-macos, use android-actions/setup-android

--- scripts/ci_checks/android/macos.sh
+++ scripts/ci_checks/android/macos.sh
@@ -2,4 +2,16 @@
 
 set -euxo pipefail
 
+export PATH="$ANDROID_HOME/ndk/${NDK_VERSION}/toolchains/llvm/prebuilt/darwin-x86_64/bin:$PATH"
+
 brew install scons ragel gengetopt
+
+scons -Q \
+      --enable-werror \
+      --enable-tests \
+      --disable-soversion \
+      --disable-tools \
+      --disable-openssl \
+      --build-3rdparty=libuv,openfec,speexdsp,cpputest \
+      --compiler=clang \
+      --host="$TOOLCHAIN"
