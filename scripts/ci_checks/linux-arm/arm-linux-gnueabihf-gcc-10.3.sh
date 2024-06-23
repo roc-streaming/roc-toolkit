9fb71daa34afc666d2ab2b1361489af70dc9af07
Andrew Petelin <adrianopol@rbox.me>
2024-06-23 17:31:24 +0300

    gh-636: added new toolchains for ARM+GCC
    New toolcnains:
    - aarch64-linux-gnu-gcc-10.3
    - arm-linux-gnueabihf-gcc-7.4
    - arm-linux-gnueabihf-gcc-10.3
    
    (cherry picked from commit 6f682867b9b6b12b44de06490a1c6f6789666f36)

--- scripts/ci_checks/linux-arm/arm-linux-gnueabihf-gcc-10.3.sh
+++ scripts/ci_checks/linux-arm/arm-linux-gnueabihf-gcc-10.3.sh
@@ -2,15 +2,15 @@
 
 set -eux -o pipefail
 
-toolchain="aarch64-linux-gnu"
-compiler="gcc-7.4.1-release"
-cpu="cortex-a53" # armv8
+toolchain="arm-none-linux-gnueabihf"
+compiler="gcc-10.3.1-release"
+cpu="cortex-a15" # armv7
 
 scons -Q \
     --enable-werror \
     --enable-tests \
     --enable-examples \
-    --build-3rdparty=all,pulseaudio:10.0 \
+    --build-3rdparty=all,pulseaudio:8.0 \
     --host=${toolchain}
 
 "$( dirname "$0" )"/run-tests-in-qemu.sh "$toolchain" "$compiler" "$cpu"
--- scripts/ci_checks/linux-arm/arm-linux-gnueabihf-gcc-7.4.sh
+++ scripts/ci_checks/linux-arm/arm-linux-gnueabihf-gcc-7.4.sh
@@ -2,15 +2,15 @@
 
 set -eux -o pipefail
 
-toolchain="aarch64-linux-gnu"
+toolchain="arm-linux-gnueabihf"
 compiler="gcc-7.4.1-release"
-cpu="cortex-a53" # armv8
+cpu="cortex-a15" # armv7
 
 scons -Q \
     --enable-werror \
     --enable-tests \
     --enable-examples \
-    --build-3rdparty=all,pulseaudio:10.0 \
+    --build-3rdparty=all,pulseaudio:8.0 \
     --host=${toolchain}
 
 "$( dirname "$0" )"/run-tests-in-qemu.sh "$toolchain" "$compiler" "$cpu"
