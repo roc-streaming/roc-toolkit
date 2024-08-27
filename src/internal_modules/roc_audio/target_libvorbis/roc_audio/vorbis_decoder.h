2a54aaa90a84acc1efbe4a35626ab8497a0e2464
Leonardo Invernizzi <lminvernizzi@gmail.com>
2024-08-27 03:40:53 +0100

    Fix

--- src/internal_modules/roc_audio/target_libvorbis/roc_audio/vorbis_decoder.h
+++ src/internal_modules/roc_audio/target_libvorbis/roc_audio/vorbis_decoder.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024 Roc Streaming authors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 //! @file roc_audio/target_libvorbis/roc_audio/vorbis_decoder.h
 //! @brief Vorbis audio decoder.
 
