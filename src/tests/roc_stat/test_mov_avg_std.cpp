1adf46fbca2c6775bd1a5139837137a561656288
Victor Gaydov <victor@enise.org>
2025-02-07 17:25:49 +0900

    chore: Split MovAggregate into MovAvgStd and MovMinMax
    Rationale:
    
     - All code that use MovAggregate actually used either avg/var/std,
       or min/max, but never together.
    
     - Implementation of avg/var/std and min/max is independent
       (two different algorithms, two different set of members).
    
     - MovHistogram and MovQuantile anyway have separate ring buffers,
       not shared with MovAggregate. If we want to share ring buffers,
       we need to invent some more generic mechanism when all MovXxx
       components will be able to reuse same buffer.

--- src/tests/roc_stat/test_mov_avg_std.cpp
+++ src/tests/roc_stat/test_mov_avg_std.cpp
@@ -6,27 +6,30 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include <CppUTest/TestHarness.h>
+#include "test_harness.h"
 
 #include "roc_core/fast_random.h"
 #include "roc_core/heap_arena.h"
 #include "roc_core/macro_helpers.h"
-#include "roc_stat/mov_aggregate.h"
+#include "roc_stat/mov_avg_std.h"
 
 namespace roc {
 namespace stat {
 
-TEST_GROUP(mov_aggregate) {
+TEST_GROUP(mov_avg_std) {
     core::HeapArena arena;
 };
 
-TEST(mov_aggregate, single_pass) {
+TEST(mov_avg_std, single_pass) {
     const size_t n = 10;
-    int64_t x[n];
-    MovAggregate<int64_t> agg(arena, n);
+    int64_t x[n] = {};
+
+    MovAvgStd<int64_t> comp(arena, n);
+    CHECK(comp.is_valid());
+
     for (size_t i = 0; i < n; i++) {
         x[i] = int64_t(i * n);
-        agg.add(x[i]);
+        comp.add(x[i]);
         double target_avg = 0;
         for (size_t j = 0; j <= i; ++j) {
             target_avg += double(x[j]);
@@ -38,39 +41,42 @@ TEST(mov_aggregate, single_pass) {
         }
         target_var = target_var / double(i + 1);
         double target_std = sqrt(target_var);
-        LONGS_EQUAL((int64_t)round(target_avg), agg.mov_avg());
-        LONGS_EQUAL((int64_t)round(target_var), agg.mov_var());
-        LONGS_EQUAL((int64_t)round(target_std), agg.mov_std());
+        LONGS_EQUAL((int64_t)round(target_avg), comp.mov_avg());
+        LONGS_EQUAL((int64_t)round(target_var), comp.mov_var());
+        LONGS_EQUAL((int64_t)round(target_std), comp.mov_std());
     }
 }
 
-TEST(mov_aggregate, one_n_half_pass) {
+TEST(mov_avg_std, one_n_half_pass) {
     const size_t n = 10;
-    MovAggregate<int64_t> agg(arena, n);
+
+    MovAvgStd<int64_t> comp(arena, n);
+    CHECK(comp.is_valid());
+
     for (size_t i = 0; i < (n * 10 + n / 2); i++) {
         const int64_t x = (int64_t)pow(-1., (double)i);
-        agg.add(x);
+        comp.add(x);
     }
 
-    LONGS_EQUAL(0, agg.mov_avg());
-    LONGS_EQUAL(1, agg.mov_var());
+    LONGS_EQUAL(0, comp.mov_avg());
+    LONGS_EQUAL(1, comp.mov_var());
 
     double target_avg = double(n - 1) * n / 2;
     double target_var = 0;
     for (size_t i = 0; i < n; i++) {
         const int64_t x = int64_t(i * n);
-        agg.add(x);
+        comp.add(x);
         target_var += (x - target_avg) * (x - target_avg);
     }
     target_var /= double(n);
     double target_std = sqrt(target_var);
 
-    LONGS_EQUAL((int64_t)round(target_avg), agg.mov_avg());
-    LONGS_EQUAL((int64_t)round(target_var), agg.mov_var());
-    LONGS_EQUAL((int64_t)round(target_std), agg.mov_std());
+    LONGS_EQUAL((int64_t)round(target_avg), comp.mov_avg());
+    LONGS_EQUAL((int64_t)round(target_var), comp.mov_var());
+    LONGS_EQUAL((int64_t)round(target_std), comp.mov_std());
 }
 
-TEST(mov_aggregate, stress_test) {
+TEST(mov_avg_std, stress_test) {
     enum { NumIterations = 10, NumElems = 1000, MinWindow = 1, MaxWindow = 100 };
 
     const int64_t ranges[][2] = {
@@ -83,8 +89,8 @@ TEST(mov_aggregate, stress_test) {
         for (size_t i = 0; i < NumIterations; i++) {
             const size_t win_sz = core::fast_random_range(MinWindow, MaxWindow);
 
-            MovAggregate<int64_t> agg(arena, win_sz);
-            CHECK(agg.is_valid());
+            MovAvgStd<int64_t> comp(arena, win_sz);
+            CHECK(comp.is_valid());
 
             int64_t elems[NumElems] = {};
 
@@ -92,7 +98,7 @@ TEST(mov_aggregate, stress_test) {
                 elems[n] = ranges[r][0]
                     + (int64_t)core::fast_random_range(
                                0, (uint64_t)(ranges[r][1] - ranges[r][0]));
-                agg.add(elems[n]);
+                comp.add(elems[n]);
 
                 const size_t n_elems = n + 1;
 
@@ -109,9 +115,9 @@ TEST(mov_aggregate, stress_test) {
                 }
                 double target_std = sqrt(target_var);
 
-                DOUBLES_EQUAL(target_avg, agg.mov_avg(), 1);
-                DOUBLES_EQUAL(target_var, agg.mov_var(), 100);
-                DOUBLES_EQUAL(target_std, agg.mov_std(), 100);
+                DOUBLES_EQUAL(target_avg, comp.mov_avg(), 1);
+                DOUBLES_EQUAL(target_var, comp.mov_var(), 100);
+                DOUBLES_EQUAL(target_std, comp.mov_std(), 100);
             }
         }
     }
