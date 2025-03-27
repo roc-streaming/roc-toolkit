9ea401a12829cb65af4ed6fc62d61dd01750ef0f
Victor Gaydov <victor@enise.org>
2025-03-28 03:35:25 +0900

    ci: Minor fixes in docker.sh

--- scripts/ci_checks/docker.sh
+++ scripts/ci_checks/docker.sh
@@ -6,7 +6,10 @@
 #
 # (the full command line with exact arguments to docker.sh can be taken from CI)
 
-set -eux -o pipefail
+set -eu -o pipefail
+
+user="${DOCKER_UID:-${UID:-`id -u`}}:${DOCKER_GID:-${GID:-`id -g`}}"
+pwd="$(pwd)"
 
 if [ -z "${CI:-}" ]; then
     opts=-ti
@@ -14,15 +17,11 @@ else
     opts=-t
 fi
 
-user="${DOCKER_UID:-"$UID"}"
-if [ -n "${DOCKER_GID:-}" ]; then
-    user="$user:$DOCKER_GID"
-fi
-
+set -x
 docker run --rm ${opts} \
        --cap-add SYS_PTRACE \
        -u "$user" \
-       -v "$(pwd)":/opt/roc \
-       -w /opt/roc \
+       -v "$pwd:/opt/roc" \
+       -w "/opt/roc" \
        -e CI="${CI:-}" \
        "$@"
