9981c5934c4a80c19b3129dff9846c07dec4092a
flw5469 <tonyfong@gmail.com>
2026-02-21 02:52:28 +0800

    add macos specific code

--- src/internal_modules/roc_core/target_nosem/roc_core/semaphore.cpp
+++ src/internal_modules/roc_core/target_nosem/roc_core/semaphore.cpp
@@ -27,10 +27,14 @@ Semaphore::Semaphore(unsigned counter)
     if (int err = pthread_condattr_init(&attr)) {
         roc_panic("semaphore: pthread_condattr_init(): %s", errno_to_str(err).c_str());
     }
+
+#if defined(CLOCK_MONOTONIC) && !defined(__APPLE__) && !defined(__MACH__)
     if (int err = pthread_condattr_setclock(&attr, CLOCK_MONOTONIC)) {
         roc_panic("semaphore: pthread_condattr_setclock(): %s",
                   errno_to_str(err).c_str());
     }
+#endif
+
     if (int err = pthread_cond_init(&cond_, &attr)) {
         roc_panic("semaphore: pthread_cond_init(): %s", errno_to_str(err).c_str());
     }
@@ -41,11 +45,34 @@ Semaphore::Semaphore(unsigned counter)
 }
 
 Semaphore::~Semaphore() {
+    /* Ensure that signal() and broadcast() are not using condvar.
+     */
     while (guard_) {
         cpu_relax();
     }
 
-    int err = 0;
+    int err;
+
+#if defined(__APPLE__) && defined(__MACH__)
+    if ((err = pthread_mutex_lock(&mutex_.mutex_))) {
+        roc_panic("mutex: pthread_mutex_lock(): %s", errno_to_str(err).c_str());
+    }
+
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = 1;
+
+    err = pthread_cond_timedwait_relative_np(&cond_, &mutex_.mutex_, &ts);
+    if (err != 0 && err != ETIMEDOUT) {
+        roc_panic("mutex: pthread_cond_timedwait_relative_np(): %s",
+                  errno_to_str(err).c_str());
+    }
+
+    if ((err = pthread_mutex_unlock(&mutex_.mutex_))) {
+        roc_panic("mutex: pthread_mutex_unlock(): %s", errno_to_str(err).c_str());
+    }
+#endif
+
     if ((err = pthread_cond_destroy(&cond_))) {
         roc_panic("sem: pthread_cond_destroy(): %s", errno_to_str(err).c_str());
     }
@@ -57,15 +84,29 @@ bool Semaphore::timed_wait(nanoseconds_t deadline) {
     }
 
     struct timespec ts;
-    ts.tv_sec = time_t(deadline / Second);
-    ts.tv_nsec = long(deadline % Second);
-    // roc_log(LogDebug, "loop top, now=%lld, deadline=%lld",
-    // core::timestamp(core::ClockMonotonic), deadline);
-
     int err = 0;
+
     mutex_.lock();
     while (err == 0 && counter_ == 0) {
+#if defined(__APPLE__) && defined(__MACH__)
+        // On macOS, convert absolute deadline to relative timeout
+        nanoseconds_t now = timestamp(ClockMonotonic);
+        if (deadline <= now) {
+            err = ETIMEDOUT;
+            break;
+        }
+        nanoseconds_t timeout = deadline - now;
+        
+        ts.tv_sec = time_t(timeout / Second);
+        ts.tv_nsec = long(timeout % Second);
+        
+        err = pthread_cond_timedwait_relative_np(&cond_, &mutex_.mutex_, &ts);
+#else
+        ts.tv_sec = time_t(deadline / Second);
+        ts.tv_nsec = long(deadline % Second);
+        
         err = pthread_cond_timedwait(&cond_, &mutex_.mutex_, &ts);
+#endif
     }
 
     bool acquired = (counter_ > 0);
@@ -73,6 +114,10 @@ bool Semaphore::timed_wait(nanoseconds_t deadline) {
         counter_--;
     }
     mutex_.unlock();
+
+    if (err != 0 && err != ETIMEDOUT) {
+        roc_panic("semaphore: pthread_cond_timedwait(): %s", errno_to_str(err).c_str());
+    }
 
     return acquired;
 }
